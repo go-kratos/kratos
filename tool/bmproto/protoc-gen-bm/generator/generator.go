f89b006eaaa6bde81da4e5ac8e926ca4b21f24a0
zhaoshichen <zhaoshichen1@gmail.com>
2019-04-30 16:27:16 +0800

    remove pkg/api, modify protoc-gen-bm/generator

--- tool/bmproto/protoc-gen-bm/generator/generator.go
+++ tool/bmproto/protoc-gen-bm/generator/generator.go
@@ -123,8 +123,8 @@ func (t *bm) generateImports(file *descriptor.FileDescriptorProto) {
 	//t.P(`	`,t.pkgs["context"], ` "context"`)
 	t.P(`	"context"`)
 	t.P()
-	t.P(`	bm "github.com/bilibili/kratos/tool/bmproto/pkg/net/http/blademaster"`)
-	t.P(`	"github.com/bilibili/kratos/tool/bmproto/pkg/net/http/blademaster/binding"`)
+	t.P(`	bm "github.com/bilibili/kratos/pkg/net/http/blademaster"`)
+	t.P(`	"github.com/bilibili/kratos/pkg/net/http/blademaster/binding"`)
 
 	t.P(`)`)
 	// It's legal to import a message and use it as an input or output for a
