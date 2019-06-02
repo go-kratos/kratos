4afe0b1389bef0f4f8d5a6427bdd287e2a92de5e
Tony <i@tony.wiki>
2019-06-02 19:07:58 +0800

    remove gogo source

--- tool/kratos-protoc/grpc.go
+++ tool/kratos-protoc/grpc.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	_getGRPCGen = "go get github.com/gogo/protobuf/protoc-gen-gofast"
-	_grpcProtoc = `protoc --proto_path=%s --proto_path=%s --proto_path=%s --gofast_out=plugins=grpc,Mgithub.com/gogo/protobuf/gogoproto/gogo.proto=github.com/bilibili/kratos/third_party/github.com/gogo/protobuf/gogoproto:.`
+	_grpcProtoc = `protoc --proto_path=%s --proto_path=%s --proto_path=%s --gofast_out=plugins=grpc:.`
 )
 
 func installGRPCGen() error {
