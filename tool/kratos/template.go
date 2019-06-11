fea77e434fc4335887dc6e45536ba854dd29da52
Tony <i@tony.wiki>
2019-06-11 19:05:46 +0800

    update kratos to v0.1.0

--- tool/kratos/template.go
+++ tool/kratos/template.go
@@ -542,7 +542,7 @@ type Kratos struct {
 go 1.12
 
 require (
-	github.com/bilibili/kratos v0.0.0-20190417043059-a08e0f771592
+	github.com/bilibili/kratos v0.1.0
 	github.com/gogo/protobuf v1.2.1
 	github.com/golang/protobuf v1.3.1
 	golang.org/x/net v0.0.0-20190420063019-afa5a82059c6
