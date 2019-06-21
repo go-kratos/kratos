6e4ab6cf3c0d80a040f77c380ebc9ddf096b40d8
Tony <i@tony.wiki>
2019-06-21 10:18:33 +0800

    fix kratos update

--- tool/kratos-protoc/bm.go
+++ tool/kratos-protoc/bm.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	_getBMGen = "go get github.com/bilibili/kratos/tool/protobuf/protoc-gen-bm"
+	_getBMGen = "go get -u github.com/bilibili/kratos/tool/protobuf/protoc-gen-bm"
 	_bmProtoc = "protoc --proto_path=%s --proto_path=%s --proto_path=%s --bm_out=explicit_http=true:."
 )
 
--- tool/kratos-protoc/swagger.go
+++ tool/kratos-protoc/swagger.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	_getSwaggerGen = "go get github.com/bilibili/kratos/tool/protobuf/protoc-gen-bswagger"
+	_getSwaggerGen = "go get -u github.com/bilibili/kratos/tool/protobuf/protoc-gen-bswagger"
 	_swaggerProtoc = "protoc --proto_path=%s --proto_path=%s --proto_path=%s --bswagger_out=explicit_http=true:."
 )
 
