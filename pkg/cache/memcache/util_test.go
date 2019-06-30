2ff467477040ed89c7d4cd7783f008759092a1f0
wei cheng <tomwei7g@gmail.com>
2019-06-30 23:38:57 +0800

    update memcache library
    - support read timeout from context
    - fix miss prometheus metrics on some method

--- pkg/cache/memcache/util_test.go
+++ pkg/cache/memcache/util_test.go
@@ -1,7 +1,9 @@
 package memcache
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	pb "github.com/bilibili/kratos/pkg/cache/memcache/test"
 
@@ -72,4 +74,32 @@ func TestLegalKey(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestShrinkDeadline(t *testing.T) {
+	t.Run("test not deadline", func(t *testing.T) {
+		timeout := time.Second
+		timeoutTime := time.Now().Add(timeout)
+		tm := shrinkDeadline(context.Background(), timeout)
+		assert.True(t, tm.After(timeoutTime))
+	})
+	t.Run("test big deadline", func(t *testing.T) {
+		timeout := time.Second
+		timeoutTime := time.Now().Add(timeout)
+		deadlineTime := time.Now().Add(2 * time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		defer cancel()
+
+		tm := shrinkDeadline(ctx, timeout)
+		assert.True(t, tm.After(timeoutTime) && tm.Before(deadlineTime))
+	})
+	t.Run("test small deadline", func(t *testing.T) {
+		timeout := time.Second
+		deadlineTime := time.Now().Add(500 * time.Millisecond)
+		ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+		defer cancel()
+
+		tm := shrinkDeadline(ctx, timeout)
+		assert.True(t, tm.After(deadlineTime) && tm.Before(time.Now().Add(timeout)))
+	})
 }
