9eeead2a3859be5a4ff0937ffe335999c386eda9
shunza <shunza@shunza.MacMini>
2019-07-22 13:50:39 +0800

    fix ut error.

--- pkg/stat/metric/gauge_test.go
+++ pkg/stat/metric/gauge_test.go
@@ -21,32 +21,3 @@ func TestGaugeSet(t *testing.T) {
 	val := gauge.Value()
 	assert.Equal(t, val, int64(50))
 }
-
-func TestGaugeVec(t *testing.T) {
-	gaugeVec := NewGaugeVec(&GaugeVecOpts{
-		Namespace: "test",
-		Subsystem: "test",
-		Name:      "test",
-		Help:      "this is test metrics.",
-		Labels:    []string{"name", "addr"},
-	})
-	gaugeVec.Set(float64(22.33), "name1", "127.0.0.1")
-	assert.Panics(t, func() {
-		NewGaugeVec(&GaugeVecOpts{
-			Namespace: "test",
-			Subsystem: "test",
-			Name:      "test",
-			Help:      "this is test metrics.",
-			Labels:    []string{"name", "addr"},
-		})
-	}, "Expected to panic.")
-	assert.NotPanics(t, func() {
-		NewGaugeVec(&GaugeVecOpts{
-			Namespace: "test",
-			Subsystem: "test",
-			Name:      "test2",
-			Help:      "this is test metrics.",
-			Labels:    []string{"name", "addr"},
-		})
-	}, "Expected normal. no panic.")
-}
