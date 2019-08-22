966361c7dbddfd0808c1f318f12dab438f0067d1
felixhao <guanguan241@gmail.com>
2019-08-22 08:39:25 +0000

    add grpc gzip

--- pkg/net/rpc/warden/server.go
+++ pkg/net/rpc/warden/server.go
@@ -10,16 +10,16 @@ import (
 	"sync"
 	"time"
 
-	"github.com/bilibili/kratos/pkg/conf/dsn"
-	"github.com/bilibili/kratos/pkg/log"
-	nmd "github.com/bilibili/kratos/pkg/net/metadata"
-	"github.com/bilibili/kratos/pkg/net/rpc/warden/ratelimiter"
-	"github.com/bilibili/kratos/pkg/net/trace"
-	xtime "github.com/bilibili/kratos/pkg/time"
+	"go-common/library/conf/dsn"
+	"go-common/library/log"
+	nmd "go-common/library/net/metadata"
+	"go-common/library/net/rpc/warden/ratelimiter"
+	"go-common/library/net/trace"
+	xtime "go-common/library/time"
 
 	//this package is for json format response
-	_ "github.com/bilibili/kratos/pkg/net/rpc/warden/internal/encoding/json"
-	"github.com/bilibili/kratos/pkg/net/rpc/warden/internal/status"
+	_ "go-common/library/net/rpc/warden/internal/encoding/json"
+	"go-common/library/net/rpc/warden/internal/status"
 
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
@@ -27,6 +27,8 @@ import (
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/reflection"
+
+	_ "google.golang.org/grpc/encoding/gzip"
 )
 
 var (
@@ -110,6 +112,7 @@ func (s *Server) handle() grpc.UnaryServerInterceptor {
 		var t trace.Trace
 		cmd := nmd.MD{}
 		if gmd, ok := metadata.FromIncomingContext(ctx); ok {
+			t, _ = trace.Extract(trace.GRPCFormat, gmd)
 			for key, vals := range gmd {
 				if nmd.IsIncomingKey(key) {
 					cmd[key] = vals[0]
@@ -225,6 +228,11 @@ func (s *Server) SetConfig(conf *ServerConfig) (err error) {
 	return nil
 }
 
+// Config return the server's config.
+func (s *Server) Config() ServerConfig {
+	return *s.conf
+}
+
 // interceptor is a single interceptor out of a chain of many interceptors.
 // Execution is done in left-to-right order, including passing of context.
 // For example ChainUnaryServer(one, two, three) will execute one before two before three, and three
@@ -304,7 +312,6 @@ func (s *Server) Start() (*Server, error) {
 	if err != nil {
 		return nil, err
 	}
-	log.Info("warden: start grpc listen addr: %v", lis.Addr())
 	reflection.Register(s.server)
 	go func() {
 		if err := s.Serve(lis); err != nil {
