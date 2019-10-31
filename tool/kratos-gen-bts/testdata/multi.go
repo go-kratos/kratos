707422e869d46a804f936a6b925fb85cbaea7269
小旭 <wangxu01@bilibili.com>
2019-10-31 15:32:38 +0800

    fixed test

--- tool/kratos-gen-bts/testdata/multi.go
+++ tool/kratos-gen-bts/testdata/multi.go
@@ -12,37 +12,37 @@ var (
 )
 
 // CacheDemos .
-func (d *Dao) CacheDemos(c context.Context, keys []int64) (map[int64]*Demo, error) {
+func (d *dao) CacheDemos(c context.Context, keys []int64) (map[int64]*Demo, error) {
 	// get data from cache
 	return _multiCacheFunc(c, keys)
 }
 
 // RawDemos .
-func (d *Dao) RawDemos(c context.Context, keys []int64) (map[int64]*Demo, error) {
+func (d *dao) RawDemos(c context.Context, keys []int64) (map[int64]*Demo, error) {
 	// get data from db
 	return _multiRawFunc(c, keys)
 }
 
 // AddCacheDemos .
-func (d *Dao) AddCacheDemos(c context.Context, values map[int64]*Demo) error {
+func (d *dao) AddCacheDemos(c context.Context, values map[int64]*Demo) error {
 	// add to cache
 	return _multiAddCacheFunc(c, values)
 }
 
 // CacheDemos1 .
-func (d *Dao) CacheDemos1(c context.Context, keys []int64) (map[int64]*Demo, error) {
+func (d *dao) CacheDemos1(c context.Context, keys []int64) (map[int64]*Demo, error) {
     // get data from cache
     return _multiCacheFunc(c, keys)
 }
 
 // RawDemos .
-func (d *Dao) RawDemos1(c context.Context, keys []int64) (map[int64]*Demo, error) {
+func (d *dao) RawDemos1(c context.Context, keys []int64) (map[int64]*Demo, error) {
     // get data from db
     return _multiRawFunc(c, keys)
 }
 
 // AddCacheDemos .
-func (d *Dao) AddCacheDemos1(c context.Context, values map[int64]*Demo) error {
+func (d *dao) AddCacheDemos1(c context.Context, values map[int64]*Demo) error {
     // add to cache
     return _multiAddCacheFunc(c, values)
 }
