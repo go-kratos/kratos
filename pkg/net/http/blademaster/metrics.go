e9b67bbe7774783ed552523ec1c3572c593cfc7e
shunza <yashun.shunza@gmail.com>
2019-12-12 10:12:59 +0800

    net http metrics add uri method.

--- pkg/net/http/blademaster/metrics.go
+++ pkg/net/http/blademaster/metrics.go
@@ -13,7 +13,7 @@ var (
 		Subsystem: "requests",
 		Name:      "duration_ms",
 		Help:      "http server requests duration(ms).",
-		Labels:    []string{"path", "caller"},
+		Labels:    []string{"path", "caller", "method"},
 		Buckets:   []float64{5, 10, 25, 50, 100, 250, 500, 1000},
 	})
 	_metricServerReqCodeTotal = metric.NewCounterVec(&metric.CounterVecOpts{
@@ -21,21 +21,21 @@ var (
 		Subsystem: "requests",
 		Name:      "code_total",
 		Help:      "http server requests error count.",
-		Labels:    []string{"path", "caller", "code"},
+		Labels:    []string{"path", "caller", "method", "code"},
 	})
 	_metricServerBBR = metric.NewCounterVec(&metric.CounterVecOpts{
 		Namespace: serverNamespace,
 		Subsystem: "",
 		Name:      "bbr_total",
 		Help:      "http server bbr total.",
-		Labels:    []string{"url"},
+		Labels:    []string{"url", "method"},
 	})
 	_metricClientReqDur = metric.NewHistogramVec(&metric.HistogramVecOpts{
 		Namespace: clientNamespace,
 		Subsystem: "requests",
 		Name:      "duration_ms",
 		Help:      "http client requests duration(ms).",
-		Labels:    []string{"path"},
+		Labels:    []string{"path", "method"},
 		Buckets:   []float64{5, 10, 25, 50, 100, 250, 500, 1000},
 	})
 	_metricClientReqCodeTotal = metric.NewCounterVec(&metric.CounterVecOpts{
@@ -43,6 +43,6 @@ var (
 		Subsystem: "requests",
 		Name:      "code_total",
 		Help:      "http client requests code count.",
-		Labels:    []string{"path", "code"},
+		Labels:    []string{"path", "method", "code"},
 	})
 )
