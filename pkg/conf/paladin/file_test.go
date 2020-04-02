2d720d408b4a651176369c80ff4a92a9f4ad10da
chenzhihui <chenzhihui@bilibili.com>
2020-04-03 00:55:37 +0800

    fix test conf

--- pkg/conf/paladin/file_test.go
+++ pkg/conf/paladin/file_test.go
@@ -71,6 +71,14 @@ func TestFileEvent(t *testing.T) {
 	// test data
 	path := "/tmp/test_conf_event/"
 	assert.Nil(t, os.MkdirAll(path, 0700))
+	assert.Nil(t, ioutil.WriteFile(filepath.Join(path, "test.toml"), []byte(`
+		text = "hello"	
+		number = 100
+	`), 0644))
+	assert.Nil(t, ioutil.WriteFile(filepath.Join(path, "abc.toml"), []byte(`
+		text = "hello"	
+		number = 100
+	`), 0644))
 	// test client
 	cli, err := NewFile(path)
 	assert.Nil(t, err)
