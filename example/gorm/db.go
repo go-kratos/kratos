d5d7c3573fffbf828256e7a9e5e040acd62f5868
Anbr <gie777@qq.com>
2020-05-03 16:54:17 +0800

    fix 测试

--- example/gorm/db.go
+++ example/gorm/db.go
@@ -1,25 +1,24 @@
-package dao
+// package dao
 
-import (
-	"github.com/go-kratos/kratos/databases/orm"
-	"github.com/jinzhu/gorm"
+// import (
+// 	"github.com/go-kratos/kratos/pkg/conf/paladin"
+// 	"github.com/go-kratos/kratos/pkg/orm"
+// 	"github.com/jinzhu/gorm"
+// )
 
-	"github.com/go-kratos/kratos/pkg/conf/paladin"
-)
-
-func NewDB() (db *gorm.DB, cf func(), err error) {
-	var (
-		cfg orm.Config
-		ct  paladin.TOML
-	)
-	if err = paladin.Get("db.toml").Unmarshal(&ct); err != nil {
-		return
-	}
-	if err = ct.Get("Client").UnmarshalTOML(&cfg); err != nil {
-		return
-	}
-	db = orm.NewMySQL(&cfg)
-	db.LogMode(true)
-	cf = func() { db.Close() }
-	return
-}
+// func NewDB() (db *gorm.DB, cf func(), err error) {
+// 	var (
+// 		cfg orm.Config
+// 		ct  paladin.TOML
+// 	)
+// 	if err = paladin.Get("db.toml").Unmarshal(&ct); err != nil {
+// 		return
+// 	}
+// 	if err = ct.Get("Client").UnmarshalTOML(&cfg); err != nil {
+// 		return
+// 	}
+// 	db = orm.NewMySQL(&cfg)
+// 	db.LogMode(true)
+// 	cf = func() { db.Close() }
+// 	return
+// }
