30efd06ec6641842bf0cb0902c5cf69de9c1ae7f
chenzhihui <zhihui_chen@foxmail.com>
2020-11-27 22:06:18 +0800

    add config interfaces

---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"expvar"
+	"time"
+)
+
+// MD is the value metadata.
+type MD interface {
+	Key() string
+}
+
+// Value is the config value interface.
+type Value interface {
+	Metadata() MD
+
+	Bool(def bool) bool
+	Int(def int) int
+	String(def string) string
+	Float64(def float64) float64
+	Duration(def time.Duration) time.Duration
+
+	Scan(val interface{}) error
+}
+
+// Watcher is the config watcher.
+type Watcher interface {
+	Next() (Value, error)
+}
+
+// Paladin is an interface abstraction for configuration.
+type Paladin interface {
+	Var(v expvar.Var) error
+	Value(key string) Value
+	Watch(key ...string) (Watcher, error)
+}
