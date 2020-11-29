4d158855844dcd8e551c4c0ae9848bdeb94d6d46
chenzhihui <zhihui_chen@foxmail.com>
2020-11-29 22:09:54 +0800

    add metadata context

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -1,1 +1,99 @@
 package metadata
+
+import (
+	"context"
+	"fmt"
+	"strings"
+)
+
+// MD is the transport layer request and response headers.
+type MD map[string][]string
+
+// New creates an MD from a given key-value map.
+func New(m map[string]string) MD {
+	md := MD{}
+	for k, val := range m {
+		key := strings.ToLower(k)
+		md[key] = append(md[key], val)
+	}
+	return md
+}
+
+// Pairs returns an MD formed by the mapping of key, value ...
+// Pairs panics if len(kv) is odd.
+func Pairs(kv ...string) MD {
+	if len(kv)%2 == 1 {
+		panic(fmt.Sprintf("metadata: Pairs got the odd number of input pairs for metadata: %d", len(kv)))
+	}
+	md := MD{}
+	var key string
+	for i, s := range kv {
+		if i%2 == 0 {
+			key = strings.ToLower(s)
+			continue
+		}
+		md[key] = append(md[key], s)
+	}
+	return md
+}
+
+// Len returns the number of items in md.
+func (md MD) Len() int {
+	return len(md)
+}
+
+// Copy returns a copy of md.
+func (md MD) Copy() MD {
+	return Join(md)
+}
+
+// Get obtains the values for a given key.
+func (md MD) Get(k string) []string {
+	k = strings.ToLower(k)
+	return md[k]
+}
+
+// Set sets the value of a given key with a slice of values.
+func (md MD) Set(k string, vals ...string) {
+	if len(vals) == 0 {
+		return
+	}
+	k = strings.ToLower(k)
+	md[k] = vals
+}
+
+// Append adds the values to key k, not overwriting what was already stored at that key.
+func (md MD) Append(k string, vals ...string) {
+	if len(vals) == 0 {
+		return
+	}
+	k = strings.ToLower(k)
+	md[k] = append(md[k], vals...)
+}
+
+// Join joins any number of mds into a single MD.
+func Join(mds ...MD) MD {
+	out := MD{}
+	for _, md := range mds {
+		for k, v := range md {
+			out[k] = append(out[k], v...)
+		}
+	}
+	return out
+}
+
+type mdKey struct{}
+
+// NewContext creates a new context with transport md attached.
+func NewContext(ctx context.Context, md MD) context.Context {
+	return context.WithValue(ctx, mdKey{}, md)
+}
+
+// FromContext returns the transport metadata in ctx if it exists.
+func FromContext(ctx context.Context) (MD, bool) {
+	md, ok := ctx.Value(mdKey{}).(MD)
+	if !ok {
+		return nil, false
+	}
+	return md, true
+}
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,1 +1,19 @@
 package middleware
+
+import "context"
+
+// Endpoint is the server endpoint.
+type Endpoint func(ctx context.Context, request interface{}) (response interface{}, err error)
+
+// Middleware is transport middleware.
+type Middleware func(Endpoint) Endpoint
+
+// Chain is the middleare function for the given pattern.
+func Chain(outer Middleware, others ...Middleware) Middleware {
+	return func(next Endpoint) Endpoint {
+		for i := len(others) - 1; i >= 0; i-- {
+			next = others[i](next)
+		}
+		return outer(next)
+	}
+}
