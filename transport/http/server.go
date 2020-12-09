1536f31e6d034928dc0d50ca71adce967fc03443
chenzhihui <zhihui_chen@foxmail.com>
2020-12-09 19:35:51 +0800

    Implements http handlers

--- transport/http/server.go
+++ transport/http/server.go
@@ -14,6 +14,8 @@ var _ transport.Server = new(Server)
 type Server struct {
 	*http.Server
 
+	handlers []http.Handler
+
 	opts serverOptions
 }
 
@@ -23,8 +25,9 @@ func NewServer(opts ...ServerOption) *Server {
 	for _, o := range opts {
 		o(&options)
 	}
-	srv := &http.Server{}
-	return &Server{Server: srv, opts: options}
+	s := &Server{opts: options}
+	s.Server = &http.Server{Handler: s}
+	return s
 }
 
 // Start start the HTTP server.
@@ -36,7 +39,19 @@ func (s *Server) Start(ctx context.Context) error {
 	return s.Serve(lis)
 }
 
-// Stop stop the HTT.
+// Stop stop the HTTP server.
 func (s *Server) Stop(ctx context.Context) error {
 	return s.Shutdown(ctx)
+}
+
+// AddHandler add a HTTP handler.
+func (s *Server) AddHandler(h http.Handler) {
+	s.handlers = append(s.handlers, h)
+}
+
+// ServeHTTP implements http.Handler.
+func (s *Server) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	for _, h := range s.handlers {
+		h.ServeHTTP(res, req)
+	}
 }
