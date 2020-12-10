42d22595ad93233b4d8d82b43f182cf1e9506c5f
weisd <weishidavip@163.com>
2020-12-10 15:53:37 +0800

    添加jaegerEnv配置

--- pkg/net/trace/jaeger/config.go
+++ pkg/net/trace/jaeger/config.go
@@ -1,11 +1,24 @@
 package jaeger
 
 import (
+	"flag"
+	"os"
+
 	"github.com/go-kratos/kratos/pkg/conf/env"
 	"github.com/go-kratos/kratos/pkg/net/trace"
 )
 
+var _jaegerEndpoint = "http://127.0.0.1:9191"
+
+func init() {
+	if v := os.Getenv("JAEGER_ENDPOINT"); v != "" {
+		_jaegerEndpoint = v
+	}
+	flag.StringVar(&_jaegerEndpoint, "jaeger_endpoint", _jaegerEndpoint, "jaeger report endpoint, or use JAEGER_ENDPOINT env.")
+}
+
+// Init Init
 func Init() {
-	c := &Config{Endpoint: "http://127.0.0.1:9191", BatchSize: 120}
+	c := &Config{Endpoint: _jaegerEndpoint, BatchSize: 120}
 	trace.SetGlobalTracer(trace.NewTracer(env.AppID, newReport(c), true))
 }
