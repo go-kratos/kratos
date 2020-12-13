7b208657c8d3f85456679591d4c03a6d1af9bf2a
chenzhihui <zhihui_chen@foxmail.com>
2020-12-13 19:47:29 +0800

    fix server info

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -2,12 +2,17 @@ package middleware
 
 import (
 	"context"
-
-	"github.com/go-kratos/kratos/v2/transport"
 )
 
 // Handler defines the handler invoked by Middleware.
 type Handler func(ctx context.Context, req interface{}) (interface{}, error)
 
+// ServerInfo is the server request infomation.
+type ServerInfo interface {
+	Server() interface{}
+	Path() string
+	Method() string
+}
+
 // Middleware is HTTP/gRPC transport middleware.
-type Middleware func(ctx context.Context, req interface{}, info *transport.ServerInfo, handler Handler) (resp interface{}, err error)
+type Middleware func(ctx context.Context, req interface{}, info *ServerInfo, handler Handler) (resp interface{}, err error)
