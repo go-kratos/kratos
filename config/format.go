e117ccd18a11ecbb8673eb7a089eb490b40e30c6
Windfarer <windfarer@gmail.com>
2020-12-17 19:25:17 +0800

    apply toml

--- config/format.go
+++ config/format.go
@@ -1,20 +1,14 @@
 package config
 
 import (
+	"encoding/json"
 	"strings"
 
 	"github.com/ghodss/yaml"
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
+	"github.com/pelletier/go-toml"
 )
-
-func ApplyYAML(yml string, pb proto.Message) error {
-	js, err := yaml.YAMLToJSON([]byte(yml))
-	if err != nil {
-		return err
-	}
-	return ApplyJSON(string(js), pb)
-}
 
 func ApplyJSON(js string, pb proto.Message) error {
 	reader := strings.NewReader(js)
@@ -25,4 +19,21 @@ func ApplyJSON(js string, pb proto.Message) error {
 		return m.Unmarshal(reader, pb)
 	}
 	return nil
+}
+
+func ApplyYAML(yml string, pb proto.Message) error {
+	js, err := yaml.YAMLToJSON([]byte(yml))
+	if err != nil {
+		return err
+	}
+	return ApplyJSON(string(js), pb)
+}
+
+func ApplyTOML(tm string, pb proto.Message) error {
+	tree, err := toml.Load(tm)
+	if err != nil {
+		return err
+	}
+	js, err := json.Marshal(tree.ToMap())
+	return ApplyJSON(string(js), pb)
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/ghodss/yaml v1.0.0
 	github.com/golang/protobuf v1.4.1
 	github.com/gorilla/mux v1.8.0
+	github.com/pelletier/go-toml v1.8.1 // indirect
 	github.com/stretchr/testify v1.6.1
 	golang.org/x/sync v0.0.0-20190423024810-112230192c58
 	google.golang.org/grpc v1.33.2
