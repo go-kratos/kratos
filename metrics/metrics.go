e05083bed7a29f8e3c117cfbed292af699442a75
chenzhihui <zhihui_chen@foxmail.com>
2021-01-06 23:31:08 +0800

    refactor metrics

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -1,39 +1,17 @@
 package metrics
 
-// Opts contains the common arguments for creating Metric.
-type Opts struct {
+// Counter is metrics counter.
+type Counter interface {
+	Add(delta float64, lvs ...string)
 }
 
-// Metric is a sample interface.
-// Implementations of Metrics in metric package are Counter, Gauge,
-// PointGauge, RollingCounter and RollingGauge.
-type Metric interface {
-	// Add adds the given value to the counter.
-	Add(int64)
-	// Value gets the current value.
-	// If the metric's type is PointGauge, RollingCounter, RollingGauge,
-	// it returns the sum value within the window.
-	Value() int64
+// Gauge is metrics gauge.
+type Gauge interface {
+	Set(value float64, lvs ...string)
+	Add(delta float64, lvs ...string)
 }
 
-// Aggregation contains some common aggregation function.
-// Each aggregation can compute summary statistics of window.
-type Aggregation interface {
-	// Min finds the min value within the window.
-	Min() float64
-	// Max finds the max value within the window.
-	Max() float64
-	// Avg computes average value within the window.
-	Avg() float64
-	// Sum computes sum value within the window.
-	Sum() float64
-}
-
-// VectorOpts contains the common arguments for creating vec Metric..
-type VectorOpts struct {
-	Namespace string
-	Subsystem string
-	Name      string
-	Help      string
-	Labels    []string
+// Histogram is metrics histogram.
+type Histogram interface {
+	Observe(value float64, lvs ...string)
 }
