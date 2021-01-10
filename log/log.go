76231b7017de51d10f3ebbae1b42372ffbb295a3
chenzhihui <zhihui_chen@foxmail.com>
2021-01-10 15:52:45 +0800

    fix logger with

--- log/log.go
+++ log/log.go
@@ -5,16 +5,16 @@ type Logger interface {
 	Print(level Level, kvpair ...interface{})
 }
 
-type prefix struct {
+type suffix struct {
 	log    Logger
 	kvpair []interface{}
 }
 
-func (l *prefix) Print(level Level, kvpair ...interface{}) {
-	l.log.Print(level, []interface{}{l.kvpair, kvpair}...)
+func (l *suffix) Print(level Level, kvpair ...interface{}) {
+	l.log.Print(level, append(kvpair, l.kvpair...)...)
 }
 
 // With with logger kv pairs.
 func With(l Logger, kvpair ...interface{}) Logger {
-	return &prefix{log: l, kvpair: kvpair}
+	return &suffix{log: l, kvpair: kvpair}
 }
