de81a91c3457e6d74be6be5da5bd6a2d00ab6b94
guoliangcao <guoliangcao@tencent.com>
2021-01-16 11:31:10 +0800

    fix discovery

--- registry/registry.go
+++ registry/registry.go
@@ -2,16 +2,34 @@ package registry
 
 import "context"
 
+// Action is service discovery action.
+type Action int
+
+const (
+	// ActionAll contains full service instances
+	ActionAll Action = 0
+	// ActionAdd contains service instances needs to be added
+	ActionAdd Action = 0
+	// ActionDel contains service instances needs to be deleted
+	ActionDel Action = 0
+	// ActionUpdate contains partial service instances needs to be updated
+	ActionUpdate Action = 0
+)
+
 // Registry is registry interface.
 type Registry interface {
 	Register(ctx context.Context, svc Service) error
 	Deregister(ctx context.Context, svc Service) error
+}
+
+// Discovery is service discovery interface.
+type Discovery interface {
 	GetService(ctx context.Context, name string) ([]Service, error)
 	Watch(name string, o Observer) error
 }
 
 // Observer is watch observer.
-type Observer func(action string, svc Service)
+type Observer func(action Action, svc []Service)
 
 // Service is service interface.
 type Service interface {
