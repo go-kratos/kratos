ad97e72c1553177ca8af4ff1938b8f4356bdf892
chenzhihui <zhihui_chen@foxmail.com>
2021-02-18 20:14:36 +0800

    add validator middleware

---
+++ middleware/validate/validate.go
@@ -0,0 +1,26 @@
+package validate
+
+import (
+	"context"
+
+	"github.com/go-kratos/kratos/v2/errors"
+	"github.com/go-kratos/kratos/v2/middleware"
+)
+
+type validator interface {
+	Validate() error
+}
+
+// Validator is a validator middleware.
+func Validator() middleware.Middleware {
+	return func(handler middleware.Handler) middleware.Handler {
+		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+			if v, ok := req.(validator); ok {
+				if err := v.Validate(); err != nil {
+					return nil, errors.InvalidArgument("Validator", err.Error())
+				}
+			}
+			return handler(ctx, req)
+		}
+	}
+}
