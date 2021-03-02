77c778af3d969a354fabc08b71cda50722b22fc5
chenzhihui <zhihui_chen@foxmail.com>
2021-03-02 23:39:49 +0800

    remove unused method

--- transport/http/binding/proto.go
+++ transport/http/binding/proto.go
@@ -18,16 +18,6 @@ import (
 	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
-// MapProto parses map params to proto message.
-func MapProto(msg proto.Message, values map[string]string) error {
-	for key, value := range values {
-		if err := populateFieldValues(msg.ProtoReflect(), strings.Split(key, "."), []string{value}); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 func mapProto(msg proto.Message, values map[string][]string) error {
 	for key, values := range values {
 		if err := populateFieldValues(msg.ProtoReflect(), strings.Split(key, "."), values); err != nil {
