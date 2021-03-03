bc62ef0bd2100fe3230e4dcb5ba9acff53830cbb
chenzhihui <zhihui_chen@foxmail.com>
2021-03-03 19:59:32 +0800

    fix generate streaming

--- cmd/protoc-gen-go-http/go.mod
+++ cmd/protoc-gen-go-http/go.mod
@@ -3,9 +3,9 @@ module github.com/go-kratos/kratos/cmd/protoc-gen-go-http/v2
 go 1.15
 
 require (
-	github.com/go-kratos/kratos/v2 v2.0.0-20210302150104-7340d4de6bee
+	github.com/go-kratos/kratos/v2 v2.0.0-alpha5
 	github.com/golang/protobuf v1.4.3
-	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/mux v1.8.0 // indirect
 	google.golang.org/genproto v0.0.0-20210202153253-cf70463f6119
 	google.golang.org/grpc v1.36.0
 	google.golang.org/protobuf v1.25.0
--- cmd/protoc-gen-go-http/http.go
+++ cmd/protoc-gen-go-http/http.go
@@ -43,6 +43,7 @@ func generateFileContent(gen *protogen.Plugin, file *protogen.File, g *protogen.
 	g.P("// is compatible with the kratos package it is being compiled against.")
 	g.P("// ", contextPackage.Ident(""), httpPackage.Ident(""), muxPackage.Ident(""))
 	g.P("const _ = ", transportPackage.Ident("SupportPackageIsVersion1"))
+	g.P("var _ = new(", contextPackage.Ident("Context"), ")")
 	g.P()
 
 	for _, service := range file.Services {
@@ -62,22 +63,24 @@ func genService(gen *protogen.Plugin, file *protogen.File, g *protogen.Generated
 		Metadata:    file.Desc.Path(),
 	}
 	for _, method := range service.Methods {
+		if method.Desc.IsStreamingClient() || method.Desc.IsStreamingServer() {
+			continue
+		}
 		rule, ok := proto.GetExtension(method.Desc.Options(), annotations.E_Http).(*annotations.HttpRule)
 		if rule != nil && ok {
 			for _, bind := range rule.AdditionalBindings {
-				sd.Methods = append(sd.Methods, buildHTTPRule(method, bind))
+				sd.Methods = append(sd.Methods, buildHTTPRule(g, method, bind))
 			}
-			sd.Methods = append(sd.Methods, buildHTTPRule(method, rule))
+			sd.Methods = append(sd.Methods, buildHTTPRule(g, method, rule))
 		} else {
 			path := fmt.Sprintf("/%s/%s", service.Desc.FullName(), method.Desc.Name())
-			sd.Methods = append(sd.Methods, buildMethodDesc(method, "POST", path))
-
+			sd.Methods = append(sd.Methods, buildMethodDesc(g, method, "POST", path))
 		}
 	}
 	g.P(sd.execute())
 }
 
-func buildHTTPRule(m *protogen.Method, rule *annotations.HttpRule) *methodDesc {
+func buildHTTPRule(g *protogen.GeneratedFile, m *protogen.Method, rule *annotations.HttpRule) *methodDesc {
 	var (
 		path         string
 		method       string
@@ -106,7 +109,7 @@ func buildHTTPRule(m *protogen.Method, rule *annotations.HttpRule) *methodDesc {
 	}
 	body = rule.Body
 	responseBody = rule.ResponseBody
-	md := buildMethodDesc(m, method, path)
+	md := buildMethodDesc(g, m, method, path)
 	if body == "*" {
 		body = ""
 	}
@@ -122,13 +125,13 @@ func buildHTTPRule(m *protogen.Method, rule *annotations.HttpRule) *methodDesc {
 	return md
 }
 
-func buildMethodDesc(m *protogen.Method, method, path string) *methodDesc {
+func buildMethodDesc(g *protogen.GeneratedFile, m *protogen.Method, method, path string) *methodDesc {
 	defer func() { methodSets[m.GoName]++ }()
 	return &methodDesc{
 		Name:    m.GoName,
 		Num:     methodSets[m.GoName],
-		Request: m.Input.GoIdent.GoName,
-		Reply:   m.Output.GoIdent.GoName,
+		Request: g.QualifiedGoIdent(m.Input.GoIdent),
+		Reply:   g.QualifiedGoIdent(m.Output.GoIdent),
 		Path:    path,
 		Method:  method,
 		Vars:    buildPathVars(m, path),
