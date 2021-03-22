24db8d965b5625fba0812f356fd2451613369704
chenzhihui <zhihui_chen@foxmail.com>
2021-03-22 18:53:56 +0800

    rename to sender & receiver

--- event/event.go
+++ event/event.go
@@ -4,36 +4,32 @@ import (
 	"context"
 )
 
-// Message is an absctraction for all messages that
+// Event is an absctraction for all messages that
 // are sent to quque or received from queue.
-type Message struct {
-	Key    string
-	Value  []byte
-	Header map[string]string
-}
-
-// Event given to a subscription handler for processing.
-type Event interface {
-	Message() *Message
-	Ack() error
-	Nack() error
+type Event struct {
+	// Key sets the key of the message for routing policy
+	Key string
+	// Payload for the message
+	Payload []byte
+	// Properties attach application defined properties on the message
+	Properties map[string]string
 }
 
 // Handler is a callback function that processes messages delivered
 // to asynchronous subscribers.
 type Handler func(context.Context, Event) error
 
-// Publisher is absctraction for sending messages
+// Sender is absctraction for sending messages
 // to queue.
-type Publisher interface {
-	Publish(ctx context.Context, msg *Message) error
-	PublishAsync(ctx context.Context, msg *Message, callback func(err error)) error
+type Sender interface {
+	Send(ctx context.Context, event Event) error
+	SendAsync(ctx context.Context, event Event, callback func(err error)) error
 	Close() error
 }
 
-// Subscriber is an absctraction for receiving messages
+// Receiver is an absctraction for receiving messages
 // from queue.
-type Subscriber interface {
-	Subscribe(ctx context.Context, h Handler) error
+type Receiver interface {
+	Receive(ctx context.Context, h Handler) error
 	Close() error
 }
