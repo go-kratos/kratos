a8824a61979662a06056423751c329646635bb84
chenzhihui <zhihui_chen@foxmail.com>
2021-04-13 22:41:58 +0800

    add check result

--- transport/http/health/health.go
+++ transport/http/health/health.go
@@ -2,7 +2,7 @@ package health
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
@@ -15,45 +15,35 @@ type Checker interface {
 	CheckHealth(ctx context.Context) error
 }
 
-// Option is a health option.
-type Option func(*Handler)
-
-// WithChecker with health checker.
-func WithChecker(c ...Checker) Option {
-	return func(o *Handler) {
-		o.checkers = c
-	}
-}
-
 // Handler is an HTTP handler that reports on the success of an
 // aggregate of Checkers.  The zero value is always healthy.
 type Handler struct {
-	checkers []Checker
+	checkers map[string]Checker
 }
 
 // NewHandler new a health handler.
-func NewHandler(opts ...Option) *Handler {
-	h := &Handler{}
-	for _, o := range opts {
-		o(h)
-	}
-	return h
+func NewHandler() *Handler {
+	return &Handler{}
 }
 
 // Add adds a new check to the handler.
-func (h *Handler) Add(c Checker) {
-	h.checkers = append(h.checkers, c)
+func (h *Handler) Add(name string, c Checker) {
+	h.checkers[name] = c
 }
 
 // ServeHTTP returns 200 if it is healthy, 500 otherwise.
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	for _, c := range h.checkers {
+	code := http.StatusOK
+	res := make(map[string]string, len(h.checkers))
+	for name, c := range h.checkers {
 		if err := c.CheckHealth(r.Context()); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprintf(w, "unhealthy: %s", err)
-			return
+			code = http.StatusInternalServerError
+			res[name] = err.Error()
+		} else {
+			res[name] = "ok"
 		}
 	}
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "ok")
+	w.WriteHeader(code)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
 }
