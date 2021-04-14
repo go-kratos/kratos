b88db2a533de89f59f635e288d63ef2ea9d8a4aa
Cluas <Cluas@live.cn>
2021-04-14 13:54:14 +0800

    registry: fix typo (#835)

--- registry/registry.go
+++ registry/registry.go
@@ -20,12 +20,12 @@ type Discovery interface {
 
 // Watcher is service watcher.
 type Watcher interface {
-	// Watch returns services in the following two cases:
+	// Next returns services in the following two cases:
 	// 1.the first time to watch and the service instance list is not empty.
 	// 2.any service instance changes found.
 	// if the above two conditions are not met, it will block until context deadline exceeded or canceled
 	Next() ([]*ServiceInstance, error)
-	// Close close the watcher.
+	// Stop close the watcher.
 	Stop() error
 }
 
