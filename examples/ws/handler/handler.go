cb68659cff217b62a7e8468dcf42205cafb40dca
miya <miyaweb@163.com>
2021-04-22 11:03:56 +0800

    ws demo (#853)
    * examples/ws: ws demo

---
+++ examples/ws/handler/handler.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func WsHandler(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
---
+++ examples/ws/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"github.com/go-kratos/kratos/examples/ws/handler"
+	"github.com/go-kratos/kratos/v2"
+	transhttp "github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/ws", handler.WsHandler)
+
+	httpSrv := transhttp.NewServer(transhttp.Address(":8080"))
+	httpSrv.HandlePrefix("/", router)
+
+	app := kratos.New(
+		kratos.Name("ws"),
+		kratos.Server(
+			httpSrv,
+		),
+	)
+	if err := app.Run(); err != nil {
+		log.Println(err)
+	}
+}
