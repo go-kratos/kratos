4f1d2f056f37492c247315259a915db7ae1d0059
opensite <lucky9303@163.com>
2021-04-26 22:48:39 +0800

    feat:add grpc reflection register (#865)

--- transport/grpc/server.go
+++ transport/grpc/server.go
@@ -14,6 +14,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/reflection"
 )
 
 const loggerName = "transport/grpc"
@@ -104,6 +105,8 @@ func NewServer(opts ...ServerOption) *Server {
 	srv.Server = grpc.NewServer(grpcOpts...)
 	// grpc health register
 	healthpb.RegisterHealthServer(srv.Server, srv.health)
+	// reflection register
+	reflection.Register(srv.Server)
 	return srv
 }
 
