6d6174652c566aa91390c7e565c39e3465872186
包子 <baozhecheng@foxmail.com>
2021-04-27 14:50:17 +0800

    fix middleware/logging unable to print params
    fix middleware/logging unable to print params

--- middleware/logging/logging.go
+++ middleware/logging/logging.go
@@ -1,4 +1,4 @@
-package logging
+package server
 
 import (
 	"context"
@@ -11,97 +11,177 @@ import (
 	"github.com/go-kratos/kratos/v2/transport/http"
 )
 
+// Option is HTTP logging option.
+type Option func(*options)
+
+type options struct {
+	logger log.Logger
+}
+
+// WithLogger with middleware logger.
+func WithLogger(logger log.Logger) Option {
+	return func(o *options) {
+		o.logger = logger
+	}
+}
+
 // Server is an server logging middleware.
-func Server(l log.Logger) middleware.Middleware {
-	logger := log.NewHelper("middleware/logging", l)
+func Server(opts ...Option) middleware.Middleware {
+	options := options{
+		logger: log.DefaultLogger,
+	}
+	for _, o := range opts {
+		o(&options)
+	}
+	log := log.NewHelper("middleware/logging", options.logger)
 	return func(handler middleware.Handler) middleware.Handler {
 		return func(ctx context.Context, req interface{}) (interface{}, error) {
 			var (
 				path      string
 				method    string
-				params    string
+				args      string
 				component string
+				query     string
 			)
 			if info, ok := http.FromServerContext(ctx); ok {
 				component = "HTTP"
-				path = info.Request.RequestURI
+				path = info.Request.URL.Path
 				method = info.Request.Method
-				params = info.Request.Form.Encode()
+				args = req.(fmt.Stringer).String()
+				query = info.Request.URL.RawQuery
 			} else if info, ok := grpc.FromServerContext(ctx); ok {
 				component = "gRPC"
 				path = info.FullMethod
 				method = "POST"
-				params = req.(fmt.Stringer).String()
+				args = req.(fmt.Stringer).String()
 			}
 			reply, err := handler(ctx, req)
-			if err != nil {
-				logger.Errorw(
+			if component == "HTTP" {
+				if err != nil {
+					log.Errorw(
+						"kind", "server",
+						"component", component,
+						"path", path,
+						"method", method,
+						"args", args,
+						"query", query,
+						"code", errors.Code(err),
+						"error", err.Error(),
+					)
+					return nil, err
+				}
+				log.Infow(
 					"kind", "server",
 					"component", component,
 					"path", path,
 					"method", method,
-					"params", params,
-					"code", errors.Code(err),
-					"error", err.Error(),
+					"args", args,
+					"query", query,
+					"code", 0,
 				)
-				return nil, err
+			} else {
+				if err != nil {
+					log.Errorw(
+						"kind", "server",
+						"component", component,
+						"path", path,
+						"method", method,
+						"args", args,
+						"code", errors.Code(err),
+						"error", err.Error(),
+					)
+					return nil, err
+				}
+				log.Infow(
+					"kind", "server",
+					"component", component,
+					"path", path,
+					"method", method,
+					"args", args,
+					"code", 0,
+				)
 			}
-			logger.Infow(
-				"kind", "server",
-				"component", component,
-				"path", path,
-				"method", method,
-				"params", params,
-				"code", 0,
-			)
 			return reply, nil
 		}
 	}
 }
 
 // Client is an client logging middleware.
-func Client(l log.Logger) middleware.Middleware {
-	logger := log.NewHelper("middleware/logging", l)
+func Client(opts ...Option) middleware.Middleware {
+	options := options{
+		logger: log.DefaultLogger,
+	}
+	for _, o := range opts {
+		o(&options)
+	}
+	log := log.NewHelper("middleware/logging", options.logger)
 	return func(handler middleware.Handler) middleware.Handler {
 		return func(ctx context.Context, req interface{}) (interface{}, error) {
 			var (
 				path      string
 				method    string
-				params    string
 				component string
+				args      string
+				query     string
 			)
 			if info, ok := http.FromClientContext(ctx); ok {
 				component = "HTTP"
-				path = info.Request.RequestURI
+				path = info.Request.URL.Path
 				method = info.Request.Method
-				params = info.Request.Form.Encode()
+				args = req.(fmt.Stringer).String()
+				query = info.Request.URL.RawQuery
 			} else if info, ok := grpc.FromClientContext(ctx); ok {
 				path = info.FullMethod
 				method = "POST"
 				component = "gRPC"
-				params = req.(fmt.Stringer).String()
+				args = req.(fmt.Stringer).String()
 			}
 			reply, err := handler(ctx, req)
-			if err != nil {
-				logger.Errorw(
+			if component == "HTTP" {
+				if err != nil {
+					log.Errorw(
+						"kind", "client",
+						"component", component,
+						"path", path,
+						"method", method,
+						"args", args,
+						"query", query,
+						"code", errors.Code(err),
+						"error", err.Error(),
+					)
+					return nil, err
+				}
+				log.Infow(
 					"kind", "client",
 					"component", component,
 					"path", path,
 					"method", method,
-					"params", params,
-					"code", errors.Code(err),
-					"error", err.Error(),
+					"args", args,
+					"query", query,
+					"code", 0,
 				)
-				return nil, err
+			} else {
+				if err != nil {
+					log.Errorw(
+						"kind", "client",
+						"component", component,
+						"path", path,
+						"method", method,
+						"args", args,
+						"code", errors.Code(err),
+						"error", err.Error(),
+					)
+					return nil, err
+				}
+				log.Infow(
+					"kind", "client",
+					"component", component,
+					"path", path,
+					"method", method,
+					"args", args,
+					"code", 0,
+				)
 			}
-			logger.Infow(
-				"kind", "client",
-				"component", component,
-				"path", path,
-				"method", method,
-				"params", params,
-				"code", 0,
-			)
 			return reply, nil
 		}
 	}
