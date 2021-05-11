eb732c4de0000609bac52897187702f22544b120
Cluas <Cluas@live.cn>
2021-05-11 10:21:59 +0800

    internal/http: optimize support more content_type (#904)
    * internal/http: optimize support more type
    
    * internal/http: add bound case
    
    * examples: fix go.sum
    
    * examples: fix go mod

--- go.mod
+++ go.mod
@@ -10,7 +10,11 @@ require (
 	github.com/imdario/mergo v0.3.6
 	go.opentelemetry.io/otel v0.19.0
 	go.opentelemetry.io/otel/trace v0.19.0
-	golang.org/x/sync v0.0.0-20190423024810-112230192c58
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9
+	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/genproto v0.0.0-20210114201628-6edceaf6022f
 	google.golang.org/grpc v1.35.0
 	google.golang.org/protobuf v1.25.0
---
+++ internal/http/http_test.go
@@ -0,0 +1,27 @@
+package http
+
+import "testing"
+
+func TestContentSubtype(t *testing.T) {
+	tests := []struct {
+		contentType string
+		want        string
+	}{
+		{"text/html; charset=utf-8", "html"},
+		{"multipart/form-data; boundary=something", "form-data"},
+		{"application/json; charset=utf-8", "json"},
+		{"application/json", "json"},
+		{"application/xml", "xml"},
+		{"text/xml", "xml"},
+		{";text/xml", ""},
+		{"application", ""},
+	}
+	for _, test := range tests {
+		t.Run(test.contentType, func(t *testing.T) {
+			got := ContentSubtype(test.contentType)
+			if got != test.want {
+				t.Fatalf("want %v got %v", test.want, got)
+			}
+		})
+	}
+}
