3a79855445511bba2c802c6543923cbee9aa902f
longXboy <longxboyhi@gmail.com>
2021-05-26 14:18:39 +0800

    delete proto json

--- encoding/json/json.go
+++ encoding/json/json.go
@@ -5,23 +5,10 @@ import (
 	"reflect"
 
 	"github.com/go-kratos/kratos/v2/encoding"
-	"google.golang.org/protobuf/encoding/protojson"
-	"google.golang.org/protobuf/proto"
 )
 
 // Name is the name registered for the json codec.
 const Name = "json"
-
-var (
-	// MarshalOptions is a configurable JSON format marshaler.
-	MarshalOptions = protojson.MarshalOptions{
-		EmitUnpopulated: true,
-	}
-	// UnmarshalOptions is a configurable JSON format parser.
-	UnmarshalOptions = protojson.UnmarshalOptions{
-		DiscardUnknown: true,
-	}
-)
 
 func init() {
 	encoding.RegisterCodec(codec{})
@@ -31,9 +18,6 @@ func init() {
 type codec struct{}
 
 func (codec) Marshal(v interface{}) ([]byte, error) {
-	if m, ok := v.(proto.Message); ok {
-		return MarshalOptions.Marshal(m)
-	}
 	return json.Marshal(v)
 }
 
@@ -44,9 +28,6 @@ func (codec) Unmarshal(data []byte, v interface{}) error {
 			rv.Set(reflect.New(rv.Type().Elem()))
 		}
 		rv = rv.Elem()
-	}
-	if m, ok := v.(proto.Message); ok {
-		return UnmarshalOptions.Unmarshal(data, m)
 	}
 	return json.Unmarshal(data, v)
 }
