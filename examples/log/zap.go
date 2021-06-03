386d9cfd2f38000c558396cb62ca9a54fd28745f
包子 <baozhecheng@foxmail.com>
2021-06-03 21:36:35 +0800

    add examples/zap/log

---
+++ examples/log/zap.go
@@ -0,0 +1,81 @@
+package log
+
+import (
+	"fmt"
+	"github.com/go-kratos/kratos/v2/log"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"os"
+)
+
+var _ log.Logger = (*ZapLogger)(nil)
+
+type ZapLogger struct {
+	log  *zap.Logger
+	Sync func() error
+}
+
+// NewZapLogger return ZapLogger
+func NewZapLogger(encoder zapcore.EncoderConfig, level zap.AtomicLevel, opts ...zap.Option) *ZapLogger {
+	writeSyncer := getLogWriter()
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(encoder),
+		zapcore.NewMultiWriteSyncer(
+			zapcore.AddSync(os.Stdout),
+			writeSyncer,
+		), level)
+	zapLogger := zap.New(core, opts...)
+	return &ZapLogger{log: zapLogger, Sync: zapLogger.Sync}
+}
+
+// Log Implementation of logger interface
+func (l *ZapLogger) Log(level log.Level, keyvals ...interface{}) error {
+	fmt.Println(len(keyvals))
+	if len(keyvals) == 0 {
+		return nil
+	}
+	// Use zap.Sugar() when keyvals is odd
+	if len(keyvals)%2 != 0 {
+		switch level {
+		case 0:
+			l.log.Sugar().Debug(keyvals)
+		case 1:
+			l.log.Sugar().Info(keyvals)
+		case 2:
+			l.log.Sugar().Warn(keyvals)
+		case 3:
+			l.log.Sugar().Error(keyvals)
+		}
+		return nil
+	}
+
+	// Zap.Field is used when keyvals pairs appear
+	var data []zap.Field
+	for i := 0; i < len(keyvals); i += 2 {
+		data = append(data, zap.Any(fmt.Sprint(keyvals[i]), fmt.Sprint(keyvals[i+1])))
+	}
+	switch level {
+	case 0:
+		l.log.Debug("", data...)
+	case 1:
+		l.log.Info("", data...)
+	case 2:
+		l.log.Warn("", data...)
+	case 3:
+		l.log.Error("", data...)
+	}
+	return nil
+}
+
+// getLogWriter writing logs to rolling files
+func getLogWriter() zapcore.WriteSyncer {
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   "./test.log",
+		MaxSize:    10,
+		MaxBackups: 5,
+		MaxAge:     30,
+		Compress:   false,
+	}
+	return zapcore.AddSync(lumberJackLogger)
+}
---
+++ examples/log/zap_test.go
@@ -0,0 +1,35 @@
+package log
+
+import (
+	"github.com/go-kratos/kratos/v2/log"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"testing"
+)
+
+func TestZapLogger(t *testing.T) {
+	encoder := zapcore.EncoderConfig{
+		TimeKey:        "t",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		StacktraceKey:  "stack",
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.FullCallerEncoder,
+	}
+	logger := NewZapLogger(
+		encoder,
+		zap.NewAtomicLevelAt(zapcore.DebugLevel),
+		zap.AddStacktrace(
+			zap.NewAtomicLevelAt(zapcore.ErrorLevel)),
+		zap.AddCallerSkip(2),
+		zap.Development(),
+	)
+	zlog := log.NewHelper(logger)
+	zlog.Infow("name","kratos","from","bilibili")
+	defer logger.Sync()
+}
