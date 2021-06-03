4860f42637409dbc95ad84e53c512d1dfcdac12d
longxboy <longxboyhi@gmail.com>
2021-06-03 16:56:55 +0800

    add ctx for logger (#998)
    * add ctx for trace

--- log/value.go
+++ log/value.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"context"
 	"runtime"
 	"strconv"
 	"strings"
@@ -16,19 +17,19 @@ var (
 )
 
 // Valuer is returns a log value.
-type Valuer func() interface{}
+type Valuer func(ctx context.Context) interface{}
 
 // Value return the function value.
-func Value(v interface{}) interface{} {
+func Value(ctx context.Context, v interface{}) interface{} {
 	if v, ok := v.(Valuer); ok {
-		return v()
+		return v(ctx)
 	}
 	return v
 }
 
 // Caller returns returns a Valuer that returns a pkg/file:line description of the caller.
 func Caller(depth int) Valuer {
-	return func() interface{} {
+	return func(context.Context) interface{} {
 		_, file, line, _ := runtime.Caller(depth)
 		if strings.LastIndex(file, "github.com/go-kratos/kratos/log") > 0 {
 			_, file, line, _ = runtime.Caller(depth + 1)
@@ -40,15 +41,15 @@ func Caller(depth int) Valuer {
 
 // Timestamp returns a timestamp Valuer with a custom time format.
 func Timestamp(layout string) Valuer {
-	return func() interface{} {
+	return func(context.Context) interface{} {
 		return time.Now().Format(layout)
 	}
 }
 
-func bindValues(keyvals []interface{}) {
+func bindValues(ctx context.Context, keyvals []interface{}) {
 	for i := 1; i < len(keyvals); i += 2 {
 		if v, ok := keyvals[i].(Valuer); ok {
-			keyvals[i] = v()
+			keyvals[i] = v(ctx)
 		}
 	}
 }
