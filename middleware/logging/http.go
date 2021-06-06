bd516353a8f0b92db28534ead171712c7deeb95d
miyabyte <miyaweb@163.com>
2021-06-06 13:23:51 +0800

    fix middleware/log default code

--- middleware/logging/http.go
+++ middleware/logging/http.go
@@ -8,15 +8,20 @@ import (
 )
 
 func httpServerLog(logger log.Logger, ctx context.Context, args string, err error) {
+	var level = log.LevelInfo
+	var code = 200
+	var errMsg = ""
+
 	info, ok := http.FromServerContext(ctx)
 	if !ok {
 		return
 	}
-	code, errMsg := extractError(err)
-	level := log.LevelInfo
+
 	if err != nil {
 		level = log.LevelError
+		code, errMsg = extractError(err)
 	}
+
 	log.WithContext(ctx, logger).Log(level,
 		"kind", "server",
 		"component", "http",
@@ -29,15 +34,20 @@ func httpServerLog(logger log.Logger, ctx context.Context, args string, err erro
 }
 
 func httpClientLog(logger log.Logger, ctx context.Context, args string, err error) {
+	var level = log.LevelInfo
+	var code = 200
+	var errMsg = ""
+
 	info, ok := http.FromClientContext(ctx)
 	if !ok {
 		return
 	}
-	code, errMsg := extractError(err)
-	level := log.LevelInfo
+
 	if err != nil {
 		level = log.LevelError
+		code, errMsg = extractError(err)
 	}
+
 	log.WithContext(ctx, logger).Log(level,
 		"kind", "client",
 		"component", "http",
