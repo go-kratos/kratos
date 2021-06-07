3cacc085d223d304e46c08cf74b2e71442d5275f
longXboy <longxboyhi@gmail.com>
2021-06-07 16:01:26 +0800

    add metadata exmaples

---
+++ examples/metadata/client/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	pb "github.com/go-kratos/kratos/examples/helloworld/helloworld"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/metadata"
+	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	transgrpc "github.com/go-kratos/kratos/v2/transport/grpc"
+	transhttp "github.com/go-kratos/kratos/v2/transport/http"
+)
+
+func main() {
+	callHTTP()
+	callGRPC()
+}
+
+func callHTTP() {
+	logger := log.DefaultLogger
+	conn, err := transhttp.NewClient(
+		context.Background(),
+		transhttp.WithMiddleware(
+			recovery.Recovery(),
+		),
+		transhttp.WithEndpoint("127.0.0.1:8000"),
+	)
+	if err != nil {
+		panic(err)
+	}
+	client := pb.NewGreeterHTTPClient(conn)
+
+	ctx := metadata.NewOutgoingContext(context.Background(), metadata.New("kratos-extra", "2233"))
+	reply, err := client.SayHello(ctx, &pb.HelloRequest{Name: "kratos"})
+	if err != nil {
+		panic(err)
+	}
+	logger.Log(log.LevelInfo, fmt.Sprintf("[http] SayHello %s\n", reply.Message))
+}
+
+func callGRPC() {
+	logger := log.DefaultLogger
+	conn, err := transgrpc.DialInsecure(
+		context.Background(),
+		transgrpc.WithEndpoint("127.0.0.1:9000"),
+		transgrpc.WithMiddleware(
+			middleware.Chain(
+				recovery.Recovery(),
+			),
+		),
+	)
+	if err != nil {
+		panic(err)
+	}
+	client := pb.NewGreeterClient(conn)
+	ctx := metadata.NewOutgoingContext(context.Background(), metadata.New("kratos-extra", "2233"))
+	reply, err := client.SayHello(ctx, &pb.HelloRequest{Name: "kratos"})
+	if err != nil {
+		panic(err)
+	}
+	logger.Log(log.LevelInfo, fmt.Sprintf("[grpc] SayHello %+v", reply))
+}
