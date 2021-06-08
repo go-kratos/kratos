28472f47c255ab56ef35b051e1ba6502bf071c73
miyabyte <miyaweb@163.com>
2021-06-08 14:45:20 +0800

    demo mux use middleware

--- examples/http/mux/main.go
+++ examples/http/mux/main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	transhttp "github.com/go-kratos/kratos/v2/transport/http"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/home", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprint(w, "Hello Gorilla Mux!")
+
+	m := middleware.Chain(
+		recovery.Recovery(),
+		//...
+	)
+	router.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+		//can use other lib such as gin.binding : Bind(r, &req)
+		next := func(ctx context.Context, req interface{}) (interface{}, error) {
+			//call XXX_service.Func
+			fmt.Fprint(w, "Hello Gorilla Mux!")
+			return nil, nil
+		}
+		m(next)
+		_, _ = next(r.Context(), &r)
+		return
 	}).Methods("GET")
 
 	httpSrv := transhttp.NewServer(transhttp.Address(":8000"))
