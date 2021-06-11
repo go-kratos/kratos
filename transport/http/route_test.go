bd172743dfe58df215202a6a7f0d3dff42f91cfe
chenzhihui <zhihui_chen@foxmail.com>
2021-06-12 01:02:59 +0800

    fix test race

--- transport/http/route_test.go
+++ transport/http/route_test.go
@@ -63,6 +63,9 @@ func TestRoute(t *testing.T) {
 		return ctx.Returns(h(ctx, u))
 	})
 
+	if e, err := srv.Endpoint(); err != nil || e == nil {
+		t.Fatal(e, err)
+	}
 	go func() {
 		if err := srv.Start(ctx); err != nil {
 			panic(err)
