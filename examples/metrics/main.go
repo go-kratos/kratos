f7eb7b8e4c15b2fe555d064f172c43bf4f84c4b2
chenzhihui <zhihui_chen@foxmail.com>
2021-06-13 15:35:30 +0800

    remove recovery middleware

--- examples/metrics/main.go
+++ examples/metrics/main.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/go-kratos/kratos/examples/helloworld/helloworld"
 	"github.com/go-kratos/kratos/v2"
-	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/go-kratos/kratos/v2/transport/http"
 	"github.com/prometheus/client_golang/prometheus"
@@ -58,7 +57,6 @@ func main() {
 	grpcSrv := grpc.NewServer(
 		grpc.Address(":9000"),
 		grpc.Middleware(
-			recovery.Recovery(),
 			metrics.Server(
 				metrics.WithSeconds(prom.NewHistogram(_metricSeconds)),
 				metrics.WithRequests(prom.NewCounter(_metricRequests)),
@@ -68,7 +66,6 @@ func main() {
 	httpSrv := http.NewServer(
 		http.Address(":8000"),
 		http.Middleware(
-			recovery.Recovery(),
 			metrics.Server(
 				metrics.WithSeconds(prom.NewHistogram(_metricSeconds)),
 				metrics.WithRequests(prom.NewCounter(_metricRequests)),
