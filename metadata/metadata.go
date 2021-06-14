27cd6e8abfc38ae24a041bbb5c9322b67df04da5
longXboy <longxboyhi@gmail.com>
2021-06-14 09:41:41 +0800

    fix files

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -1,6 +1,8 @@
 package metadata
 
 import (
+	"context"
+	"fmt"
 	"strings"
 )
 
@@ -58,4 +60,54 @@ func (m Metadata) Clone() Metadata {
 		md[k] = v
 	}
 	return md
+}
+
+type serverMetadataKey struct{}
+
+// NewServerContext creates a new context with client md attached.
+func NewServerContext(ctx context.Context, md Metadata) context.Context {
+	return context.WithValue(ctx, serverMetadataKey{}, md)
+}
+
+// FromServerContext returns the server metadata in ctx if it exists.
+func FromServerContext(ctx context.Context) (Metadata, bool) {
+	md, ok := ctx.Value(serverMetadataKey{}).(Metadata)
+	return md, ok
+}
+
+type clientMetadataKey struct{}
+
+// NewClientContext creates a new context with client md attached.
+func NewClientContext(ctx context.Context, md Metadata) context.Context {
+	return context.WithValue(ctx, clientMetadataKey{}, md)
+}
+
+// FromClientContext returns the client metadata in ctx if it exists.
+func FromClientContext(ctx context.Context) (Metadata, bool) {
+	md, ok := ctx.Value(clientMetadataKey{}).(Metadata)
+	return md, ok
+}
+
+// AppendClientContext returns a new context with the provided kv merged
+// with any existing metadata in the context.
+func AppendClientContext(ctx context.Context, kv ...string) context.Context {
+	if len(kv)%2 == 1 {
+		panic(fmt.Sprintf("metadata: AppendToOutgoingContext got an odd number of input pairs for metadata: %d", len(kv)))
+	}
+	md, _ := FromClientContext(ctx)
+	md = md.Clone()
+	for i := 0; i < len(kv); i += 2 {
+		md.Set(kv[i], kv[i+1])
+	}
+	return NewClientContext(ctx, md)
+}
+
+// MergeClientContext merge new metadata into ctx.
+func MergeClientContext(ctx context.Context, cmd Metadata) context.Context {
+	md, _ := FromClientContext(ctx)
+	md = md.Clone()
+	for k, v := range cmd {
+		md[k] = v
+	}
+	return NewClientContext(ctx, md)
 }
