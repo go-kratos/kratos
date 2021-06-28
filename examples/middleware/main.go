fb4de63dfe4ea5435a8188486d2d5fb13cd88905
chenzhihui <zhihui_chen@foxmail.com>
2021-06-28 23:41:00 +0800

    clean middleware

--- examples/middleware/main.go
+++ examples/middleware/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/go-kratos/kratos/examples/helloworld/helloworld"
 	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/go-kratos/kratos/v2/transport/http"
 )
 
@@ -17,32 +20,41 @@ func (s *server) SayHello(ctx context.Context, in *helloworld.HelloRequest) (*he
 	return &helloworld.HelloReply{Message: "hello"}, nil
 }
 
+func authMiddleware(handler middleware.Handler) middleware.Handler {
+	return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+		log.Println("auth middleware in", req)
+		reply, err = handler(ctx, req)
+		fmt.Println("auth middleware out", reply)
+		return
+	}
+}
+
+func loggingMiddleware(handler middleware.Handler) middleware.Handler {
+	return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+		fmt.Println("logging middleware in", req)
+		reply, err = handler(ctx, req)
+		fmt.Println("logging middleware out", reply)
+		return
+	}
+}
+
 func main() {
-	s := &server{}
-	/*
-		The processing order of a request is the order of middleware registration,
-		while the processing order of the response is the reverse of the registration order.
-		         ┌───────────────────┐
-		         │MIDDLEWARE 1       │
-		         │ ┌────────────────┐│
-		         │ │MIDDLEWARE 2    ││
-		         │ │ ┌─────────────┐││
-		         │ │ │MIDDLEWARE 3 │││
-		         │ │ │ ┌─────────┐ │││
-		REQUEST  │ │ │ │  YOUR   │ │││  RESPONSE
-		   ──────┼─┼─┼─▷ HANDLER ○─┼┼┼───▷
-		         │ │ │ └─────────┘ │││
-		         │ │ └─────────────┘││
-		         │ └────────────────┘│
-		         └───────────────────┘
-	*/
 	httpSrv := http.NewServer(
 		http.Address(":8000"),
 		http.Middleware(
-			serviceMiddleware,
-			serviceMiddleware2,
+			loggingMiddleware,
+			authMiddleware,
 		),
 	)
+	grpcSrv := grpc.NewServer(
+		grpc.Address(":9000"),
+		grpc.Middleware(
+			loggingMiddleware,
+			authMiddleware,
+		),
+	)
+	s := &server{}
+	helloworld.RegisterGreeterServer(grpcSrv, s)
 	helloworld.RegisterGreeterHTTPServer(httpSrv, s)
 	app := kratos.New(
 		kratos.Name("helloworld"),
