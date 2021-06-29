95b917dd2a21b5120290e98fd46ae8dd34d78d18
包子 <baozhecheng@foxmail.com>
2021-06-29 11:46:24 +0800

    fix examples/benchmark/test.sh

--- examples/benchmark/test.sh
+++ examples/benchmark/test.sh
@@ -20,7 +20,7 @@ test_web_framework()
   ./$server_bin_name $2 $3 &
   sleep 2
 
-  throughput=`wrk -t$cpu_cores -c$4 -d30s http://127.0.0.1:8080/helloworld/benchmark | grep Requests/sec | awk '{print $2}'`
+  throughput=`wrk -t$cpu_cores -c$4 -d30s http://127.0.0.1:8080/ | grep Requests/sec | awk '{print $2}'`
   echo "throughput: $throughput requests/second"
   test_result[$1]=$throughput
 
