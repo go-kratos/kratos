74cce0ca504baaa892af223717b216213d6567c4
包子 <baozhecheng@foxmail.com>
2021-07-02 22:08:13 +0800

    feat(cmd): add kratos changelog command

---
+++ cmd/kratos/internal/change/change.go
@@ -0,0 +1,43 @@
+package change
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/go-kratos/kratos/cmd/kratos/v2/internal/base"
+	"github.com/spf13/cobra"
+)
+
+// CmdNew represents the new command.
+var CmdNew = &cobra.Command{
+	Use:   "changelog",
+	Short: "Get a kratos change log",
+	Long:  "Get a kratos release or commits info. Example: kratos changelog dev or kratos changelog {version}",
+	Run:   run,
+}
+var token string
+var repoURL string
+
+func init() {
+	if repoURL = os.Getenv("KRATOS_REPO"); repoURL == "" {
+		repoURL = "https://github.com/go-kratos/kratos.git"
+	}
+	CmdNew.Flags().StringVarP(&repoURL, "repo-url", "r", repoURL, "github repo")
+	token = os.Getenv("GITHUB_TOKEN")
+}
+
+func run(cmd *cobra.Command, args []string) {
+	owner, repo := base.ParseGithubUrl(repoURL)
+	api := base.GithubApi{Owner: owner, Repo: repo, Token: token}
+	version := "latest"
+	if len(args) > 0 {
+		version = args[0]
+	}
+	if version == "dev" {
+		info := api.GetCommitsInfo()
+		fmt.Print(base.ParseCommitsInfo(info))
+	} else {
+		info := api.GetReleaseInfo(version)
+		fmt.Print(base.ParseReleaseInfo(info))
+	}
+}
