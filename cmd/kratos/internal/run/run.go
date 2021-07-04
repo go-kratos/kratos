2e15eb330ab9a70c41ee742761ceaddb9c288a4f
包子 <baozhecheng@foxmail.com>
2021-07-04 12:44:12 +0800

    feat(cmd): kratos run command support to find the cmd directory from the parent directory

--- cmd/kratos/internal/run/run.go
+++ cmd/kratos/internal/run/run.go
@@ -34,7 +34,7 @@ func Run(cmd *cobra.Command, args []string) {
 	}
 	if dir == "" {
 		// find the directory containing the cmd/*
-		cmdDir, err := findCMD()
+		cmdDir, err := findCMD(base)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "\033[31mERROR: %s\033[m\n", err)
 			return
@@ -43,20 +43,22 @@ func Run(cmd *cobra.Command, args []string) {
 			fmt.Fprintf(os.Stderr, "\033[31mERROR: %s\033[m\n", "The cmd directory cannot be found in the current directory")
 			return
 		} else if len(cmdDir) == 1 {
-			dir = path.Join(base, cmdDir[0])
+			dir = cmdDir[0]
 		} else {
 			prompt := &survey.Select{
 				Message: "Which directory do you want to run?",
 				Options: cmdDir,
 			}
 			survey.AskOne(prompt, &dir)
-			dir = path.Join(base, dir)
 		}
 	}
 	fd := exec.Command("go", "run", ".")
 	fd.Stdout = os.Stdout
 	fd.Stderr = os.Stderr
 	fd.Dir = dir
+	if fd.Dir == "" {
+		return
+	}
 	if err := fd.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "\033[31mERROR: %s\033[m\n", err.Error())
 		return
@@ -64,9 +66,42 @@ func Run(cmd *cobra.Command, args []string) {
 	return
 }
 
-func findCMD() ([]string, error) {
+func findCMD(dir string) ([]string, error) {
+	hasCMDPaths, err := walk(dir)
+	if err != nil {
+		return []string{}, err
+	}
+	if len(hasCMDPaths) == 0 {
+		// find go.mod from the parent directory.
+		p := dir
+		d := ""
+		for {
+			d = filepath.Join(p, "..")
+			if d == p {
+				return []string{}, nil
+			}
+			paths, err := ioutil.ReadDir(p)
+			if err != nil {
+				return []string{}, err
+			}
+			for _, fileInfo := range paths {
+				if fileInfo.Name() == "go.mod" {
+					hasCMDPaths, err = walk(p)
+					if err != nil {
+						return []string{}, err
+					}
+					return hasCMDPaths, nil
+				}
+			}
+			p = d
+		}
+	}
+	return hasCMDPaths, err
+}
+
+func walk(dir string) ([]string, error) {
 	var hasCMDPaths []string
-	err := filepath.Walk(".", func(walkPath string, info os.FileInfo, err error) error {
+	err := filepath.Walk(dir, func(walkPath string, info os.FileInfo, err error) error {
 		// multi level directory is not allowed under the cmd directory, so it is judged that the path ends with cmd.
 		if strings.HasSuffix(walkPath, "cmd") {
 			paths, err := ioutil.ReadDir(walkPath)
@@ -85,5 +120,5 @@ func findCMD() ([]string, error) {
 	if err != nil {
 		return []string{}, err
 	}
-	return hasCMDPaths, err
+	return hasCMDPaths, nil
 }
