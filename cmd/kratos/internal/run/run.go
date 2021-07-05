8f9266eca4a77ba61027fb0898977902f610b67e
包子 <baozhecheng@foxmail.com>
2021-07-05 11:24:58 +0800

    fix

--- cmd/kratos/internal/run/run.go
+++ cmd/kratos/internal/run/run.go
@@ -34,7 +34,7 @@ func Run(cmd *cobra.Command, args []string) {
 	}
 	if dir == "" {
 		// find the directory containing the cmd/*
-		cmdPath, err := findCMD(base)
+		cmdDir, cmdPath, err := findCMD(base)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "\033[31mERROR: %s\033[m\n", err)
 			return
@@ -43,13 +43,17 @@ func Run(cmd *cobra.Command, args []string) {
 			fmt.Fprintf(os.Stderr, "\033[31mERROR: %s\033[m\n", "The cmd directory cannot be found in the current directory")
 			return
 		} else if len(cmdPath) == 1 {
-			dir = cmdPath[0]
+			dir = path.Join(cmdDir, cmdPath[0])
 		} else {
 			prompt := &survey.Select{
 				Message: "Which directory do you want to run?",
 				Options: cmdPath,
 			}
 			survey.AskOne(prompt, &dir)
+			if dir == "" {
+				return
+			}
+			dir = path.Join(cmdDir,dir)
 		}
 	}
 	fd := exec.Command("go", "run", ".")
@@ -63,9 +67,12 @@ func Run(cmd *cobra.Command, args []string) {
 	return
 }
 
-func findCMD(base string) ([]string, error) {
-	next := func(dir string) ([]string, error) {
-		var cmdPath []string
+func findCMD(base string) (string, []string, error) {
+	next := func(dir string) (string, []string, error) {
+		var (
+			cmdDir  string
+			cmdPath []string
+		)
 		err := filepath.Walk(dir, func(walkPath string, info os.FileInfo, err error) error {
 			// multi level directory is not allowed under the cmd directory, so it is judged that the path ends with cmd.
 			if strings.HasSuffix(walkPath, "cmd") {
@@ -75,25 +82,25 @@ func findCMD(base string) ([]string, error) {
 				}
 				for _, fileInfo := range paths {
 					if fileInfo.IsDir() {
-						cmdPath = append(cmdPath, path.Join(walkPath, fileInfo.Name()))
+						cmdPath = append(cmdPath, path.Join("cmd", fileInfo.Name()))
 					}
 				}
+				cmdDir = filepath.Join(walkPath,"..")
 				return nil
 			}
 			return nil
 		})
-		return cmdPath, err
+		return cmdDir, cmdPath, err
 	}
 	for i := 0; i < 5; i++ {
-		base = path.Clean(base)
-		res, err := next(base)
+		cmdDir, res, err := next(base)
 		if err != nil {
-			return nil, err
+			return "", nil, err
 		}
 		if len(res) > 0 {
-			return res, nil
+			return cmdDir, res, nil
 		}
-		base = path.Join(base, "..")
+		base = filepath.Join(base,"..")
 	}
-	return []string{base}, nil
+	return "", []string{}, nil
 }
