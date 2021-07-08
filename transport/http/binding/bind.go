2a0a3269b78fc1e498785218a308a6fec27b1ffb
longXboy <longxboyhi@gmail.com>
2021-07-08 13:01:48 +0800

    fix binding

--- transport/http/binding/bind.go
+++ transport/http/binding/bind.go
@@ -4,25 +4,13 @@ import (
 	"net/http"
 	"net/url"
 
+	"github.com/go-kratos/kratos/v2/encoding"
 	"github.com/go-kratos/kratos/v2/encoding/form"
-
-	gform "github.com/go-playground/form/v4"
-	"google.golang.org/protobuf/proto"
 )
-
-var decoder = gform.NewDecoder()
-
-func init() {
-	decoder.SetTagName("json")
-}
 
 // BindQuery bind vars parameters to target.
 func BindQuery(vars url.Values, target interface{}) error {
-	if msg, ok := target.(proto.Message); ok {
-		return form.MapProto(msg, vars)
-	}
-
-	return decoder.Decode(target, vars)
+	return encoding.GetCodec(form.Name).Unmarshal([]byte(vars.Encode()), target)
 }
 
 // BindForm bind form parameters to target.
@@ -30,8 +18,5 @@ func BindForm(req *http.Request, target interface{}) error {
 	if err := req.ParseForm(); err != nil {
 		return err
 	}
-	if msg, ok := target.(proto.Message); ok {
-		return form.MapProto(msg, req.Form)
-	}
-	return decoder.Decode(target, req.Form)
+	return encoding.GetCodec(form.Name).Unmarshal([]byte(req.Form.Encode()), target)
 }
