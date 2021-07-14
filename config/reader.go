c04823f22127884ec43ffe85ea342d87525d5579
Kagaya <kagaya85@outlook.com>
2021-07-14 11:19:27 +0800

    fix ci failed

--- config/reader.go
+++ config/reader.go
@@ -63,6 +63,8 @@ func (r *reader) Source() ([]byte, error) {
 func cloneMap(src map[string]interface{}) (map[string]interface{}, error) {
 	// https://gist.github.com/soroushjp/0ec92102641ddfc3ad5515ca76405f4d
 	var buf bytes.Buffer
+	gob.Register(map[string]interface{}{})
+	gob.Register([]interface{}{})
 	enc := gob.NewEncoder(&buf)
 	dec := gob.NewDecoder(&buf)
 	err := enc.Encode(src)
