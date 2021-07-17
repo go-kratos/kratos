748ec6617b1045aa1db20b22475e74a14c3621c4
包子 <baozhecheng@foxmail.com>
2021-07-17 21:49:30 +0800

    fix

--- examples/registry/etcd/server/main.go
+++ examples/registry/etcd/server/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/go-kratos/kratos/v2/middleware/recovery"
-	"github.com/go-kratos/kratos/v2/transport/http"
+
 	"log"
 
 	"github.com/go-kratos/etcd/registry"
 	pb "github.com/go-kratos/kratos/examples/helloworld/helloworld"
 	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
+	"github.com/go-kratos/kratos/v2/transport/http"
 	etcd "go.etcd.io/etcd/client/v3"
 )
 
