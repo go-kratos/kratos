808e662d504512f6517847ca53c3a73342c93d9b
chenzhihui <zhihui_chen@foxmail.com>
2021-07-28 10:28:45 +0800

    clean

--- examples/tls/client/main.go
+++ examples/tls/client/main.go
@@ -8,7 +8,6 @@ import (
 	"log"
 
 	pb "github.com/go-kratos/kratos/examples/helloworld/helloworld"
-	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/go-kratos/kratos/v2/transport/http"
@@ -46,15 +45,6 @@ func callHTTP(tlsConf *tls.Config) {
 		log.Fatal(err)
 	}
 	log.Printf("[http] SayHello %s\n", reply.Message)
-
-	// returns error
-	reply, err = client.SayHello(context.Background(), &pb.HelloRequest{Name: "error"})
-	if err != nil {
-		log.Printf("[http] SayHello error: %v\n", err)
-	}
-	if errors.IsBadRequest(err) {
-		log.Printf("[http] SayHello error is invalid argument: %v\n", err)
-	}
 }
 
 func callGRPC(tlsConf *tls.Config) {
@@ -75,13 +65,4 @@ func callGRPC(tlsConf *tls.Config) {
 		log.Fatal(err)
 	}
 	log.Printf("[grpc] SayHello %+v\n", reply)
-
-	// returns error
-	_, err = client.SayHello(context.Background(), &pb.HelloRequest{Name: "error"})
-	if err != nil {
-		log.Printf("[grpc] SayHello error: %v\n", err)
-	}
-	if errors.IsBadRequest(err) {
-		log.Printf("[grpc] SayHello error is invalid argument: %v\n", err)
-	}
 }
