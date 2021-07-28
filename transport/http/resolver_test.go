4b6ec189ca4b9f4634818a853a1b778e276a027c
chenzhihui <zhihui_chen@foxmail.com>
2021-07-29 00:57:36 +0800

    fix parseTarget ut

--- transport/http/resolver_test.go
+++ transport/http/resolver_test.go
@@ -7,15 +7,15 @@ import (
 )
 
 func TestParseTarget(t *testing.T) {
-	target, err := parseTarget("localhost:8000", false)
+	target, err := parseTarget("localhost:8000", true)
 	assert.Nil(t, err)
 	assert.Equal(t, &Target{Scheme: "http", Authority: "localhost:8000"}, target)
 
-	target, err = parseTarget("discovery:///demo", false)
+	target, err = parseTarget("discovery:///demo", true)
 	assert.Nil(t, err)
 	assert.Equal(t, &Target{Scheme: "discovery", Authority: "", Endpoint: "demo"}, target)
 
-	target, err = parseTarget("127.0.0.1:8000", false)
+	target, err = parseTarget("127.0.0.1:8000", true)
 	assert.Nil(t, err)
 	assert.Equal(t, &Target{Scheme: "http", Authority: "127.0.0.1:8000"}, target)
 
@@ -23,7 +23,7 @@ func TestParseTarget(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, &Target{Scheme: "https", Authority: "127.0.0.1:8000"}, target)
 
-	target, err = parseTarget("127.0.0.1:8000", true)
+	target, err = parseTarget("127.0.0.1:8000", false)
 	assert.Nil(t, err)
 	assert.Equal(t, &Target{Scheme: "https", Authority: "127.0.0.1:8000"}, target)
 }
