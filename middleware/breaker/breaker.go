91f4cd370e16669c18836201b7b4c82879a8678f
Kagaya <kagaya85@outlook.com>
2021-08-10 21:25:32 +0800

    add options for err info

--- middleware/breaker/breaker.go
+++ middleware/breaker/breaker.go
@@ -2,15 +2,38 @@ package breaker
 
 import (
 	"context"
-	"errors"
 
+	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/middleware"
 )
 
-// ErrBreakerTriggered is returned when the
-// circuit breaker is triggered and the request
-// is rejected directly.
-var ErrBreakerTriggered = errors.New("circuit breaker triggered")
+type Option func(*options)
+
+// WithErrorCode set error code when breaker is triggered,
+// default error code 503
+func WithErrorCode(code int) Option {
+	return func(o *options) {
+		o.errCode = code
+	}
+}
+
+func WithErrorReason(reason string) Option {
+	return func(o *options) {
+		o.errReason = reason
+	}
+}
+
+func WithErrorMessage(message string) Option {
+	return func(o *options) {
+		o.errMessage = message
+	}
+}
+
+type options struct {
+	errCode    int
+	errReason  string
+	errMessage string
+}
 
 // Breaker interface defines a circuit breaker for Handler
 type Breaker interface {
@@ -20,16 +43,28 @@ type Breaker interface {
 	// Check breaker use Check(error) to check whether
 	// the request succeeded of failed,
 	Check(err error) bool
-	// Mark wheather the current request is success
+	// Mark whether the current request is success
 	Mark(isSuccess bool)
 }
 
-func CircuitBreaker(breaker Breaker) middleware.Middleware {
+// CircuitBreaker middleware will return errBreakerTriggered when the circuit
+// breaker is triggered and the request is rejected directly.
+func CircuitBreaker(breaker Breaker, opts ...Option) middleware.Middleware {
+	options := &options{
+		errCode:    503,
+		errReason:  "circuit breaker triggered",
+		errMessage: "request failed due to circuit breaker triggered",
+	}
+	for _, o := range opts {
+		o(options)
+	}
+
+	errBreakerTriggered := errors.New(options.errCode, options.errReason, options.errMessage)
 	return func(handler middleware.Handler) middleware.Handler {
 		return func(ctx context.Context, req interface{}) (interface{}, error) {
 			if err := breaker.Allow(ctx); err != nil {
 				// reject request
-				return nil, ErrBreakerTriggered
+				return nil, errBreakerTriggered
 			}
 			// allow request
 			reply, err := handler(ctx, req)
