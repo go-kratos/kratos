d210f2d9a70be8ac0c82cdebe96b232017bd3b4d
Kagaya <kagaya85@outlook.com>
2021-08-10 21:11:55 +0800

    add options for err info

--- middleware/ratelimit/limiter.go
+++ middleware/ratelimit/limiter.go
@@ -2,14 +2,38 @@ package ratelimit
 
 import (
 	"context"
-	"errors"
 
+	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/middleware"
 )
 
-// ErrLimitExceed is returned when the rate limiter is
-// triggered and the request is rejected due to limit exceeded.
-var ErrLimitExceed = errors.New("rate limit exceeded")
+type Option func(*options)
+
+// WithErrorCode set error code when limiter triggered,
+// default error code 503
+func WithErrorCode(code int) Option {
+	return func(o *options) {
+		o.errCode = code
+	}
+}
+
+func WithErrorReason(reason string) Option {
+	return func(o *options) {
+		o.errReason = reason
+	}
+}
+
+func WithErrorMessage(message string) Option {
+	return func(o *options) {
+		o.errMessage = message
+	}
+}
+
+type options struct {
+	errCode    int
+	errReason  string
+	errMessage string
+}
 
 // Limiter limit interface.
 type Limiter interface {
@@ -17,13 +41,25 @@ type Limiter interface {
 }
 
 // RateLimiter middleware
-func RateLimiter(limiter Limiter) middleware.Middleware {
+func RateLimiter(limiter Limiter, opts ...Option) middleware.Middleware {
+	options := &options{
+		errCode:    503,
+		errReason:  "rate limit exceeded",
+		errMessage: "service unavailable due to rate limit exceeded",
+	}
+	for _, o := range opts {
+		o(options)
+	}
+
+	// errLimitExceed is returned when the rate limiter is
+	// triggered and the request is rejected due to limit exceeded.
+	errLimitExceed := errors.New(options.errCode, options.errReason, options.errMessage)
 	return func(handler middleware.Handler) middleware.Handler {
 		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
 			done, e := limiter.Allow(ctx)
 			if e != nil {
 				// blocked
-				return nil, ErrLimitExceed
+				return nil, errLimitExceed
 			}
 			// passed
 			reply, err = handler(ctx, req)
