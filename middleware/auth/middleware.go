f9ea49c4ef54e51fd09ab3a2a0253a598ec6db2e
Casper-Mars <solitaryreach@163.com>
2021-08-10 21:25:40 +0800

    feat(middleware/auth):add option to set auth header

--- middleware/auth/middleware.go
+++ middleware/auth/middleware.go
@@ -22,6 +22,20 @@ var (
 	ErrNeedTokenProvider = errors.Unauthorized("Missing info", "Token provider is missing")
 )
 
+type Option func(options *options)
+
+//options an option setting
+type options struct {
+	authHeaderKey string
+}
+
+//WithAuthHeaderKey set key that hold auth token in header
+func WithAuthHeaderKey(headerKey string) Option {
+	return func(options *options) {
+		options.authHeaderKey = headerKey
+	}
+}
+
 //TokenParser parse auth token from header
 type TokenParser interface {
 	ParseToken(token string) (interface{}, error)
@@ -33,11 +47,12 @@ type TokenProvider interface {
 }
 
 //Server is a server auth middleware
-func Server(parser TokenParser) middleware.Middleware {
+func Server(parser TokenParser, opts ...Option) middleware.Middleware {
+	o := initOptions(opts...)
 	return func(handler middleware.Handler) middleware.Handler {
 		return func(ctx context.Context, req interface{}) (interface{}, error) {
 			if header, ok := transport.FromServerContext(ctx); ok {
-				tokenInfo, err := parser.ParseToken(header.RequestHeader().Get(string(HeaderKey)))
+				tokenInfo, err := parser.ParseToken(header.RequestHeader().Get(o.authHeaderKey))
 				if err != nil {
 					return nil, err
 				}
@@ -50,17 +65,30 @@ func Server(parser TokenParser) middleware.Middleware {
 }
 
 //Client is a client jwt middleware
-func Client(provider TokenProvider) middleware.Middleware {
+func Client(provider TokenProvider, opts ...Option) middleware.Middleware {
+	o := initOptions(opts...)
 	return func(handler middleware.Handler) middleware.Handler {
 		return func(ctx context.Context, req interface{}) (interface{}, error) {
 			if provider == nil {
 				return nil, ErrNeedTokenProvider
 			}
 			if clientContext, ok := transport.FromClientContext(ctx); ok {
-				clientContext.RequestHeader().Set(string(HeaderKey), provider.GetToken())
+				clientContext.RequestHeader().Set(o.authHeaderKey, provider.GetToken())
 				return handler(ctx, req)
 			}
 			return nil, ErrWrongContext
 		}
 	}
+}
+
+//initOptions init the option
+func initOptions(opts ...Option) *options {
+	o := &options{}
+	for _, opt := range opts {
+		opt(o)
+	}
+	if o.authHeaderKey == "" {
+		o.authHeaderKey = "Authorization"
+	}
+	return o
 }
