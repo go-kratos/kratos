eaf0ceab0c179c42273fe3477079dfac83caf79a
包子 <baozhecheng@foxmail.com>
2021-08-24 11:35:26 +0800

    chore(registry): unified management of decentralized warehouses (#1368)
    * chore(registry): unified management of decentralized warehouses

---
+++ registry/zookeeper/go.mod
@@ -0,0 +1,10 @@
+module github.com/go-kratos/kratos/registry/zookeeper/v2
+
+go 1.16
+
+require (
+	github.com/go-kratos/kratos/v2 v2.0.5
+	github.com/go-zookeeper/zk v1.0.2
+)
+
+replace github.com/go-kratos/kratos/v2 => ../../../kratos
---
+++ registry/zookeeper/register_test.go
@@ -0,0 +1,71 @@
+package registry
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/go-kratos/kratos/v2/registry"
+)
+
+func TestRegistry(t *testing.T) {
+	ctx := context.Background()
+	s := &registry.ServiceInstance{
+		ID:        "0",
+		Name:      "helloworld",
+		Endpoints: []string{"http://127.0.0.1:1111"},
+	}
+
+	r, _ := New([]string{"127.0.0.1:2181"})
+
+	w, err := r.Watch(ctx, s.Name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer w.Stop()
+	go func() {
+		for {
+			res, err := w.Next()
+			if err != nil {
+				return
+			}
+			t.Logf("watch: %d", len(res))
+			for _, r := range res {
+				t.Logf("next: %+v", r)
+			}
+		}
+	}()
+	time.Sleep(time.Second)
+
+	if err := r.Register(ctx, s); err != nil {
+		t.Fatal(err)
+	}
+	time.Sleep(time.Second)
+
+	res, err := r.GetService(ctx, s.Name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i, re := range res {
+		t.Logf("first %d re:%v\n", i, re)
+	}
+	if len(res) != 1 && res[0].Name != s.Name {
+		t.Errorf("not expected: %+v", res)
+	}
+
+	if err := r.Deregister(ctx, s); err != nil {
+		t.Fatal(err)
+	}
+	time.Sleep(time.Second)
+
+	res, err = r.GetService(ctx, s.Name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i, re := range res {
+		t.Logf("second %d re:%v\n", i, re)
+	}
+	if len(res) != 0 {
+		t.Errorf("not expected empty")
+	}
+}
