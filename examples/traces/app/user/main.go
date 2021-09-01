0e7b034e36295d52cba86bd8dec66151d3eb5e56
shenqidebaozi <906604588@qq.com>
2021-09-01 15:25:47 +0800

    fix

--- examples/traces/app/user/main.go
+++ examples/traces/app/user/main.go
@@ -90,8 +90,8 @@ func (s *server) GetMyMessages(ctx context.Context, in *v1.GetMyMessagesRequest)
 
 func main() {
 	logger := log.NewStdLogger(os.Stdout)
-	logger = log.With(logger, "trace_id", tracing.TraceID())
-	logger = log.With(logger, "span_id", tracing.SpanID())
+	logger = log.With(logger, "trace_id", log.TraceID())
+	logger = log.With(logger, "span_id", log.SpanID())
 	log := log.NewHelper(logger)
 
 	url := "http://jaeger:14268/api/traces"
