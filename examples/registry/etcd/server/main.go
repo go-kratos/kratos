3a2a2f866dcafc47fbb488294f89a454d92b675f
chenzhihui <zhihui_chen@foxmail.com>
2021-09-01 13:35:24 +0800

    fix

--- examples/registry/etcd/server/main.go
+++ examples/registry/etcd/server/main.go
@@ -9,10 +9,9 @@ import (
 	pb "github.com/go-kratos/kratos/examples/helloworld/helloworld"
 	"github.com/go-kratos/kratos/v2"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
-	"github.com/go-kratos/kratos/v2/registry"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"github.com/go-kratos/kratos/v2/transport/http"
-	etcd "go.etcd.io/etcd/client/v3"
+	etcdclient "go.etcd.io/etcd/client/v3"
 )
 
 // server is used to implement helloworld.GreeterServer.
@@ -26,7 +25,7 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 }
 
 func main() {
-	client, err := etcd.New(etcd.Config{
+	client, err := etcdclient.New(etcdclient.Config{
 		Endpoints: []string{"127.0.0.1:2379"},
 	})
 	if err != nil {
@@ -50,7 +49,7 @@ func main() {
 	pb.RegisterGreeterServer(grpcSrv, s)
 	pb.RegisterGreeterHTTPServer(httpSrv, s)
 
-	r := registry.New(client)
+	r := etcd.New(client)
 	app := kratos.New(
 		kratos.Name("helloworld"),
 		kratos.Server(
