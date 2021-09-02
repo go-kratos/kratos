6e8157bdd44f4911ff7d2a814337747e41e6b187
shenqidebaozi <906604588@qq.com>
2021-09-02 11:22:12 +0800

    feat: add apollo example and some problems of fix apollo config

--- contrib/config/apollo/apollo.go
+++ contrib/config/apollo/apollo.go
@@ -4,10 +4,64 @@ import (
 	"github.com/go-kratos/kratos/v2/config"
 
 	"github.com/apolloconfig/agollo/v4"
+	apolloConfig "github.com/apolloconfig/agollo/v4/env/config"
 )
 
 type apollo struct {
 	client *agollo.Client
+}
+
+type Option func(*options)
+
+type options struct {
+	ip             string
+	appid          string
+	cluster        string
+	secret         string
+	namespaceName  string
+	isBackupConfig bool
+}
+
+// AppID with apollo config app id
+func AppID(appID string) Option {
+	return func(o *options) {
+		o.appid = appID
+	}
+}
+
+// Cluster with apollo config cluster
+func Cluster(cluster string) Option {
+	return func(o *options) {
+		o.cluster = cluster
+	}
+}
+
+// IP with apollo config conf server ip
+func IP(ip string) Option {
+	return func(o *options) {
+		o.ip = ip
+	}
+}
+
+// IsBackupConfig with apollo config isBackupConfig
+func IsBackupConfig(isBackupConfig bool) Option {
+	return func(o *options) {
+		o.isBackupConfig = isBackupConfig
+	}
+}
+
+// Secret with apollo config app secret
+func Secret(secret string) Option {
+	return func(o *options) {
+		o.secret = secret
+	}
+}
+
+// NamespaceName with apollo config namespace name
+func NamespaceName(name string) Option {
+	return func(o *options) {
+		o.namespaceName = name
+	}
 }
 
 // NewSource start with config file in ENV
@@ -17,13 +71,34 @@ type apollo struct {
 func NewSource() config.Source {
 	client, err := agollo.Start()
 	if err != nil {
-		return nil
+		panic(err)
+	}
+	return &apollo{client}
+}
+
+func NewSourceWithConfig(opts ...Option) config.Source {
+	op := options{}
+	for _, o := range opts {
+		o(&op)
+	}
+	client, err := agollo.StartWithConfig(func() (*apolloConfig.AppConfig, error) {
+		return &apolloConfig.AppConfig{
+			AppID:          op.appid,
+			Cluster:        op.cluster,
+			NamespaceName:  op.namespaceName,
+			IP:             op.ip,
+			IsBackupConfig: op.isBackupConfig,
+			Secret:         op.secret,
+		}, nil
+	})
+	if err != nil {
+		panic(err)
 	}
 	return &apollo{client}
 }
 
 func (e *apollo) load() []*config.KeyValue {
-	kv := make([]*config.KeyValue, e.client.GetDefaultConfigCache().EntryCount())
+	kv := make([]*config.KeyValue, 0)
 	e.client.GetDefaultConfigCache().Range(func(key, value interface{}) bool {
 		kv = append(kv, &config.KeyValue{
 			Key:   key.(string),
--- contrib/config/apollo/watcher.go
+++ contrib/config/apollo/watcher.go
@@ -15,10 +15,18 @@ type customChangeListener struct {
 }
 
 func (c *customChangeListener) OnChange(changeEvent *storage.ChangeEvent) {
+	kv := make([]*config.KeyValue, 0)
+	for key, value := range changeEvent.Changes {
+		kv = append(kv, &config.KeyValue{
+			Key:   key,
+			Value: []byte(value.NewValue.(string)),
+		})
+	}
+	c.event <- kv
 }
 
 func (c *customChangeListener) OnNewestChange(changeEvent *storage.FullChangeEvent) {
-	kv := make([]*config.KeyValue, len(changeEvent.Changes))
+	kv := make([]*config.KeyValue, 0)
 	for key, value := range changeEvent.Changes {
 		kv = append(kv, &config.KeyValue{
 			Key:   key,
@@ -30,13 +38,14 @@ func (c *customChangeListener) OnNewestChange(changeEvent *storage.FullChangeEve
 
 func NewWatcher(a *apollo) (config.Watcher, error) {
 	e := make(chan []*config.KeyValue)
-	a.client.AddChangeListener(&customChangeListener{})
+	a.client.AddChangeListener(&customChangeListener{event: e})
 	return &watcher{event: e}, nil
 }
 
 // Next will be blocked until the Stop method is called
 func (w *watcher) Next() ([]*config.KeyValue, error) {
-	return <-w.event, nil
+	event := <-w.event
+	return event, nil
 }
 
 func (w *watcher) Stop() error {
