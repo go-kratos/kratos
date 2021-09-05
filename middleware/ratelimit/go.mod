e18457d4d4080d58b912c8cbd6f1eea662089d04
Kagaya <kagaya85@outlook.com>
2021-09-05 18:54:46 +0800

    add bbr limiter

--- middleware/ratelimit/go.mod
+++ middleware/ratelimit/go.mod
@@ -3,8 +3,9 @@ module github.com/go-kratos/kratos/middleware/ratelimit/v2
 go 1.16
 
 require (
+	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/go-kratos/kratos/v2 v2.0.4
-	github.com/go-kratos/sra/ratelimit/bbr v0.0.0-20210811060608-030d6993323d
+	github.com/go-kratos/sra v0.0.0-20210905065551-b690f7ef1d3e
 )
 
 replace github.com/go-kratos/kratos/v2 => ../../../kratos
--- middleware/ratelimit/limiter.go
+++ middleware/ratelimit/limiter.go
@@ -5,46 +5,43 @@ import (
 
 	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/go-kratos/sra/ratelimit"
 	"github.com/go-kratos/sra/ratelimit/bbr"
 )
-
-// Limiter limit interface.
-type Limiter interface {
-	Allow(ctx context.Context) (done func(), err error)
-}
 
 type Option func(*options)
 
 // WithLimiter set Limiter implementation,
 // default is bbr limiter
-func WithLimiter(limiter Limiter) Option {
+func WithLimiter(limiter ratelimit.Limiter) Option {
 	return func(o *options) {
 		o.limiter = limiter
 	}
 }
 
 type options struct {
-	limiter Limiter
+	limiter ratelimit.Limiter
 }
 
-// RateLimiter middleware
-func RateLimiter(limiter Limiter, opts ...Option) middleware.Middleware {
+// Server ratelimiter middleware
+func Server(opts ...Option) middleware.Middleware {
 	options := &options{
 		limiter: bbr.NewLimiter(),
 	}
 	for _, o := range opts {
 		o(options)
 	}
+
 	return func(handler middleware.Handler) middleware.Handler {
 		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
-			done, e := limiter.Allow(ctx)
+			done, e := options.limiter.Allow()
 			if e != nil {
 				// rejected
 				return nil, errors.New(429, "RATELIMIT", "service unavailable due to rate limit exceeded")
 			}
 			// allowed
 			reply, err = handler(ctx, req)
-			done()
+			done(ratelimit.DoneInfo{})
 			return
 		}
 	}
