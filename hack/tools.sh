5d24a3e4f612bb3c5bfec3638e775ee205159651
Kagaya <kagaya85@outlook.com>
2021-09-06 15:55:26 +0800

    bug fixed

--- hack/tools.sh
+++ hack/tools.sh
@@ -15,26 +15,13 @@ KRAOTS_HOME=$(
 		pwd
 )
 
-echo "$KRAOTS_HOME"
-
-source "${KRAOTS_HOME}/hack/tools.sh"
+source "${KRAOTS_HOME}/hack/util.sh"
 
 LINTER=${KRAOTS_HOME}/bin/golangci-lint
 LINTER_CONFIG=${KRAOTS_HOME}/.golangci.yml
 FAILURE_FILE=${KRAOTS_HOME}/hack/.lintcheck_failures
 
-function find_modules() {
-	find . -not \( \
-		\( \
-		-path './output' \
-		-o -path './.git' \
-		-o -path '*/third_party/*' \
-		-o -path '*/vendor/*' \
-		\) -prune \
-		\) -name 'go.mod' -print0 | xargs -0 -I {} dirname {}
-}
-
-all_modules=$(find_modules)
+all_modules=$(util::find_modules)
 failing_modules=()
 while IFS='' read -r line; do failing_modules+=("$line"); done < <(cat "$FAILURE_FILE")
 
@@ -43,9 +30,8 @@ while IFS='' read -r line; do failing_modules+=("$line"); done < <(cat "$FAILURE
 function lint() {
 	for mod in $all_modules; do
 		local in_failing
-		in_failing="$(util::array_contains "$mod" "${failing_modules[*]}")"
-		echo "$mod" "$in_failing"
-		if [[ "$in_failing" -ne "1" ]]; then
+		util::array_contains "$mod" "${failing_modules[*]}" && in_failing=$? || in_failing=$?
+		if [[ "$in_failing" -ne "0" ]]; then
 			pushd "$mod" >/dev/null &&
 				echo "golangci lint $(sed -n 1p go.mod | cut -d ' ' -f2)" &&
 				eval "${LINTER} run --timeout=5m --config=${LINTER_CONFIG}"
@@ -56,9 +42,11 @@ function lint() {
 
 # test all mod
 function test() {
-	for dir in $all_modules; do
-		if [[ ! " ${failing_modules[*]} " =~ ${mod} ]]; then
-			pushd "$dir" >/dev/null &&
+	for mod in $all_modules; do
+		local in_failing
+		util::array_contains "$mod" "${failing_modules[*]}" && in_failing=$? || in_failing=$?
+		if [[ "$in_failing" -ne "0" ]]; then
+			pushd "$mod" >/dev/null &&
 				echo "go test $(sed -n 1p go.mod | cut -d ' ' -f2)" &&
 				go test ./...
 			popd >/dev/null || exit
@@ -68,9 +56,11 @@ function test() {
 
 # try to fix all mod with golangci-lint
 function fix() {
-	for dir in $all_modules; do
-		if [[ ! " ${failing_modules[*]} " =~ ${mod} ]]; then
-			pushd "$dir" >/dev/null &&
+	for mod in $all_modules; do
+		local in_failing
+		util::array_contains "$mod" "${failing_modules[*]}" && in_failing=$? || in_failing=$?
+		if [[ "$in_failing" -ne "0" ]]; then
+			pushd "$mod" >/dev/null &&
 				echo "golangci fix $(sed -n 1p go.mod | cut -d ' ' -f2)" &&
 				eval "${LINTER} run -v --fix --timeout=5m --config=${LINTER_CONFIG}"
 			popd >/dev/null || exit
@@ -79,9 +69,11 @@ function fix() {
 }
 
 function tidy() {
-	for dir in $all_modules; do
-		if [[ ! " ${failing_modules[*]} " =~ ${mod} ]]; then
-			pushd "$dir" >/dev/null &&
+	for mod in $all_modules; do
+		local in_failing
+		util::array_contains "$mod" "${failing_modules[*]}" && in_failing=$? || in_failing=$?
+		if [[ "$in_failing" -ne "0" ]]; then
+			pushd "$mod" >/dev/null &&
 				echo "go mod tidy $(sed -n 1p go.mod | cut -d ' ' -f2)" &&
 				go mod tidy
 			popd >/dev/null || exit
--- hack/util.sh
+++ hack/util.sh
@@ -5,15 +5,26 @@
 #
 
 # arguments: target, item1, item2, item3, ...
-# returns 1 if target is in the given items, 0 otherwise.
-util::array_contains() {
+# returns 0 if target is in the given items, 1 otherwise.
+function util::array_contains() {
 	local target="$1"
-	local items
 	shift
-	for items; do
-		if [[ "${items}" == "${target}" ]]; then
-			return 1
+	local items="$*"
+	for item in ${items[*]}; do
+		if [[ "${item}" == "${target}" ]]; then
+			return 0
 		fi
 	done
-	return 0
+	return 1
+}
+
+function util::find_modules() {
+	find . -not \( \
+		\( \
+		-path './output' \
+		-o -path './.git' \
+		-o -path '*/third_party/*' \
+		-o -path '*/vendor/*' \
+		\) -prune \
+		\) -name 'go.mod' -print0 | xargs -0 -I {} dirname {}
 }
