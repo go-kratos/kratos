1afed9dc0ff1ac932f9368f180ff0fcdc02bd909
Casper-Mars <solitaryreach@gmail.com>
2021-09-09 23:43:25 +0800

    feat(example/auth):change some func and remove helloworld

--- examples/auth/jwt/client/main.go
+++ examples/auth/jwt/client/main.go
@@ -4,7 +4,8 @@ import (
 	"context"
 	"log"
 
-	"github.com/go-kratos/kratos/examples/auth/helloworld"
+	"github.com/go-kratos/kratos/examples/helloworld/helloworld"
+
 	"github.com/go-kratos/kratos/v2"
 	"github.com/go-kratos/kratos/v2/middleware/auth/jwt"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
@@ -14,16 +15,26 @@ import (
 
 type server struct {
 	helloworld.UnimplementedGreeterServer
+
+	pc helloworld.GreeterClient
 }
 
 func (s *server) SayHello(ctx context.Context, in *helloworld.HelloRequest) (*helloworld.HelloReply, error) {
-	return &helloworld.HelloReply{Message: "hello from service 2"}, nil
+	return s.pc.SayHello(ctx, in)
+}
+
+type tokenProvider struct {
+	accessSecretKey string
+}
+
+func (t tokenProvider) Key() []byte {
+	return []byte(t.accessSecretKey)
 }
 
 func main() {
-	testKey := "serviceTestKey"
+	testKey := "testKey"
 	httpSrv := http.NewServer(
-		http.Address(":8001"),
+		http.Address(":8000"),
 		http.Middleware(
 			jwt.Server(func(token *jwtv4.Token) (interface{}, error) {
 				return []byte(testKey), nil
@@ -31,18 +42,33 @@ func main() {
 		),
 	)
 	grpcSrv := grpc.NewServer(
-		grpc.Address(":9001"),
+		grpc.Address(":9000"),
 		grpc.Middleware(
 			jwt.Server(func(token *jwtv4.Token) (interface{}, error) {
 				return []byte(testKey), nil
 			}),
 		),
 	)
-	s := &server{}
+	serviceTestKey := "serviceTestKey"
+	con, err := grpc.DialInsecure(
+		context.Background(),
+		grpc.WithEndpoint("dns:///127.0.0.1:9001"),
+		grpc.WithMiddleware(
+			jwt.Client(tokenProvider{
+				accessSecretKey: serviceTestKey,
+			}),
+		),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := &server{
+		pc: helloworld.NewGreeterClient(con),
+	}
 	helloworld.RegisterGreeterServer(grpcSrv, s)
 	helloworld.RegisterGreeterHTTPServer(httpSrv, s)
 	app := kratos.New(
-		kratos.Name("helloworld2"),
+		kratos.Name("helloworld"),
 		kratos.Server(
 			httpSrv,
 			grpcSrv,
--- examples/auth/jwt/server/main.go
+++ examples/auth/jwt/server/main.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"log"
 
-	"github.com/go-kratos/kratos/examples/auth/helloworld"
+	"github.com/go-kratos/kratos/examples/helloworld/helloworld"
 	"github.com/go-kratos/kratos/v2"
 	"github.com/go-kratos/kratos/v2/middleware/auth/jwt"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
@@ -14,26 +14,16 @@ import (
 
 type server struct {
 	helloworld.UnimplementedGreeterServer
-
-	pc helloworld.GreeterClient
 }
 
 func (s *server) SayHello(ctx context.Context, in *helloworld.HelloRequest) (*helloworld.HelloReply, error) {
-	return s.pc.SayHello(ctx, in)
-}
-
-type tokenProvider struct {
-	accessSecretKey string
-}
-
-func (t tokenProvider) AccessSecretKey() []byte {
-	return []byte(t.accessSecretKey)
+	return &helloworld.HelloReply{Message: "hello from service 2"}, nil
 }
 
 func main() {
-	testKey := "testKey"
+	testKey := "serviceTestKey"
 	httpSrv := http.NewServer(
-		http.Address(":8000"),
+		http.Address(":8001"),
 		http.Middleware(
 			jwt.Server(func(token *jwtv4.Token) (interface{}, error) {
 				return []byte(testKey), nil
@@ -41,33 +31,18 @@ func main() {
 		),
 	)
 	grpcSrv := grpc.NewServer(
-		grpc.Address(":9000"),
+		grpc.Address(":9001"),
 		grpc.Middleware(
 			jwt.Server(func(token *jwtv4.Token) (interface{}, error) {
 				return []byte(testKey), nil
 			}),
 		),
 	)
-	serviceTestKey := "serviceTestKey"
-	con, err := grpc.DialInsecure(
-		context.Background(),
-		grpc.WithEndpoint("dns:///127.0.0.1:9001"),
-		grpc.WithMiddleware(
-			jwt.Client(tokenProvider{
-				accessSecretKey: serviceTestKey,
-			}),
-		),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	s := &server{
-		pc: helloworld.NewGreeterClient(con),
-	}
+	s := &server{}
 	helloworld.RegisterGreeterServer(grpcSrv, s)
 	helloworld.RegisterGreeterHTTPServer(httpSrv, s)
 	app := kratos.New(
-		kratos.Name("helloworld"),
+		kratos.Name("helloworld2"),
 		kratos.Server(
 			httpSrv,
 			grpcSrv,
