fe0d0e3e6696005e298a5994c41081cde73feae7
Windfarer <windfarer@gmail.com>
2021-09-10 15:07:47 +0800

    fix lint

--- contrib/config/etcd/config_test.go
+++ contrib/config/etcd/config_test.go
@@ -5,15 +5,17 @@ import (
 	"testing"
 	"time"
 
-	"go.etcd.io/etcd/client/v3"
+	clientv3 "go.etcd.io/etcd/client/v3"
 	"google.golang.org/grpc"
 )
 
 const testKey = "/kratos/test/config"
 
 func TestConfig(t *testing.T) {
-	client, err := clientv3.New(clientv3.Config{Endpoints: []string{"127.0.0.1:2379"},
-		DialTimeout: time.Second, DialOptions: []grpc.DialOption{grpc.WithBlock()}})
+	client, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"127.0.0.1:2379"},
+		DialTimeout: time.Second, DialOptions: []grpc.DialOption{grpc.WithBlock()},
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
