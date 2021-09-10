c0ef8cc455a8033fff87302c2a23928af8a1be42
Letian Yi <letian0805@sina.com>
2021-09-10 17:36:20 +0800

    feat: add http header handler

--- transport/http/server.go
+++ transport/http/server.go
@@ -168,6 +168,11 @@ func (s *Server) HandleFunc(path string, h http.HandlerFunc) {
 	s.router.HandleFunc(path, h)
 }
 
+// HandleHeader registers a new route with a matcher for the header.
+func (s *Server) HandleHeader(key, val string, h http.HandlerFunc) {
+	s.router.Headers(key, val).Handler(h)
+}
+
 // ServeHTTP should write reply headers and data to the ResponseWriter and then return.
 func (s *Server) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	s.Handler.ServeHTTP(res, req)
