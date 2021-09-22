109462dac00c7c9c1c5176a9b383bb574866dac3
liuxiong <liuxiong332@163.com>
2021-09-22 11:35:52 +0800

    Add consul kv for config

---
+++ contrib/config/consul/watcher.go
@@ -0,0 +1,64 @@
+package etcd
+
+import (
+	"github.com/go-kratos/kratos/v2/config"
+	"github.com/hashicorp/consul/api"
+	"github.com/hashicorp/consul/api/watch"
+)
+
+type watcher struct {
+	source    *source
+	ch        chan interface{}
+	closeChan chan struct{}
+}
+
+func (w *watcher) handle(idx uint64, data interface{}) {
+	if data == nil {
+		return
+	}
+
+	_, ok := data.(api.KVPairs)
+	if !ok {
+		return
+	}
+
+	w.ch <- struct{}{}
+}
+
+func newWatcher(s *source) (*watcher, error) {
+	w := &watcher{
+		source:    s,
+		ch:        make(chan interface{}),
+		closeChan: make(chan struct{}),
+	}
+
+	wp, err := watch.Parse(map[string]interface{}{"type": "keyprefix", "prefix": s.options.path})
+
+	if err != nil {
+		return nil, err
+	}
+
+	wp.Handler = w.handle
+
+	// wp.Run is a blocking call and will prevent newWatcher from returning
+	go wp.RunWithClientAndHclog(s.client, nil)
+
+	return w, nil
+}
+
+func (s *watcher) Next() ([]*config.KeyValue, error) {
+	select {
+	case _, ok := <-s.ch:
+		if !ok {
+			return nil, nil
+		}
+		return s.source.Load()
+	case <-s.closeChan:
+		return nil, nil
+	}
+}
+
+func (s *watcher) Stop() error {
+	close(s.closeChan)
+	return nil
+}
