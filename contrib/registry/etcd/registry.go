1872c7e98e0f10447f6ad2dd85d4cb56d4c59666
zwhyb <99347745@qq.com>
2021-09-22 18:34:11 +0800

    fix:etcd key prefix contain

--- contrib/registry/etcd/registry.go
+++ contrib/registry/etcd/registry.go
@@ -122,7 +122,7 @@ func (r *Registry) GetService(ctx context.Context, name string) ([]*registry.Ser
 
 // Watch creates a watcher according to the service name.
 func (r *Registry) Watch(ctx context.Context, name string) (registry.Watcher, error) {
-	key := fmt.Sprintf("%s/%s", r.opts.namespace, name)
+	key := fmt.Sprintf("%s/%s/", r.opts.namespace, name)
 	return newWatcher(ctx, key, r.client)
 }
 
