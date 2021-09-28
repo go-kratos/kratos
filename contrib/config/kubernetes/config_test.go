9e2b5c4a5f71dc6b317e7abaf15432b532f8949d
倒霉狐狸 <mail@xiaoliu.org>
2021-09-28 11:43:09 +0800

    fix: contrib config kubernetes lint err

--- contrib/config/kubernetes/config_test.go
+++ contrib/config/kubernetes/config_test.go
@@ -1,9 +1,11 @@
 package kubernetes
 
 import (
+	"log"
 	"path/filepath"
 	"testing"
 
+	"github.com/go-kratos/kratos/v2/config"
 	"k8s.io/client-go/util/homedir"
 )
 
@@ -20,5 +22,39 @@ func TestSource(t *testing.T) {
 	}
 	for _, v := range kvs {
 		t.Log(v)
+	}
+}
+
+func ExampleNewSource() {
+	/*
+			部署在mesh namespace 下configmap
+			yamlData 示例
+			database:
+			  mysql:
+				dsn: "root:Test@tcp(mysql.database.svc.cluster.local:3306)/test?timeout=1s&readTimeout=1s&writeTimeout=1s&parseTime=true&loc=Local&charset=utf8mb4,utf8"
+				active: 20
+				idle: 10
+				idle_timeout: 3600
+			  redis:
+				addr: "redis-master.redis.svc.cluster.local:6379"
+				password: ""
+				db: 4
+
+			yamlApp 示例
+			application:
+		  		expire: 3600
+	*/
+	conf := config.New(
+		config.WithSource(
+			NewSource(
+				Namespace("mesh"),
+				LabelSelector("app=test"),
+				KubeConfig(filepath.Join(homedir.HomeDir(), ".kube", "config")),
+			),
+		),
+	)
+	err := conf.Load()
+	if err != nil {
+		log.Panic(err)
 	}
 }
