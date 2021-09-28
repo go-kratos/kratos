9fb37fc4a2b77ce1c89b45c3079b83402bc3d05d
倒霉狐狸 <mail@xiaoliu.org>
2021-09-28 14:01:28 +0800

    remove unwanted docs

--- contrib/config/kubernetes/config_test.go
+++ contrib/config/kubernetes/config_test.go
@@ -26,24 +26,6 @@ func TestSource(t *testing.T) {
 }
 
 func ExampleNewSource() {
-	/*
-			部署在mesh namespace 下configmap
-			yamlData 示例
-			database:
-			  mysql:
-				dsn: "root:Test@tcp(mysql.database.svc.cluster.local:3306)/test?timeout=1s&readTimeout=1s&writeTimeout=1s&parseTime=true&loc=Local&charset=utf8mb4,utf8"
-				active: 20
-				idle: 10
-				idle_timeout: 3600
-			  redis:
-				addr: "redis-master.redis.svc.cluster.local:6379"
-				password: ""
-				db: 4
-
-			yamlApp 示例
-			application:
-		  		expire: 3600
-	*/
 	conf := config.New(
 		config.WithSource(
 			NewSource(
