2bf7573d0fa43e9a67cef222787afd3ac9486cec
justin <leyou240@live.cn>
2021-10-18 18:15:05 +0800

    fix use of internal package github.com/go-kratos/kratos/v2/internal/testdata/encoding not allowed

--- contrib/encoding/msgpack/msgpack_test.go
+++ contrib/encoding/msgpack/msgpack_test.go
@@ -3,7 +3,6 @@ package msgpack
 import (
 	"testing"
 
-	testData "github.com/go-kratos/kratos/v2/internal/testdata/encoding"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -24,27 +23,8 @@ func TestName(t *testing.T) {
 
 func TestCodec(t *testing.T) {
 	c := new(codec)
-
-	model := testData.TestModel{
-		Id:    1,
-		Name:  "kratos",
-		Hobby: []string{"study", "eat", "play"},
-	}
-
-	m, err := c.Marshal(&model)
-	assert.Nil(t, err)
-
-	var res testData.TestModel
-
-	err = c.Unmarshal(m, &res)
-	assert.Nil(t, err)
-
-	assert.Equal(t, res.Id, model.Id)
-	assert.Equal(t, res.Name, model.Name)
-	assert.Equal(t, res.Hobby, model.Hobby)
-
 	t2 := testModel{ID: 1, Name: "name"}
-	m, err = c.Marshal(&t2)
+	m, err := c.Marshal(&t2)
 	assert.Nil(t, err)
 	var t3 testModel
 	err = c.Unmarshal(m, &t3)
