7f5981d5c6be298f8af3db826aeae18f7ae7e3dd
zwhyb <99347745@qq.com>
2021-10-21 15:49:22 +0800

    test: add log test

--- log/value_test.go
+++ log/value_test.go
@@ -1,9 +1,30 @@
 package log
 
-import "testing"
+import (
+	"context"
+	"reflect"
+	"testing"
+)
 
 func TestValue(t *testing.T) {
 	logger := DefaultLogger
 	logger = With(logger, "ts", DefaultTimestamp, "caller", DefaultCaller)
 	_ = logger.Log(LevelInfo, "msg", "helloworld")
+
+	logger2 := DefaultLogger
+	logger2 = With(logger2)
+	_ = logger.Log(LevelDebug, "msg", "helloworld")
+
+	var v1 interface{}
+	got := Value(context.Background(), v1)
+	if got != v1 {
+		t.Errorf("Value() = %v, want %v", got, v1)
+	}
+	var v2 Valuer = func(ctx context.Context) interface{} {
+		return 3
+	}
+	got = Value(context.Background(), v2)
+	if reflect.DeepEqual(got, 3) {
+		t.Errorf("Value() = %v, want %v", got, 3)
+	}
 }
