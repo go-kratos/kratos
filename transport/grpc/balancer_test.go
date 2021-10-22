218cf23d51ecdf620a9f2d7ce8b55d32b81119a3
longXboy <longxboyhi@gmail.com>
2021-10-22 12:43:14 +0800

    add transport grpc test

---
+++ transport/grpc/balancer_test.go
@@ -0,0 +1,29 @@
+package grpc
+
+import (
+	"testing"
+
+	"github.com/go-kratos/kratos/v2/selector/filter"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/metadata"
+)
+
+func TestTrailer(t *testing.T) {
+	trailer := Trailer(metadata.New(map[string]string{"a": "b"}))
+	assert.Equal(t, "b", trailer.Get("a"))
+	assert.Equal(t, "", trailer.Get("3"))
+}
+
+func TestBalancerName(t *testing.T) {
+	o := &clientOptions{}
+
+	WithBalancerName("p2c")(o)
+	assert.Equal(t, "p2c", o.balancerName)
+}
+
+func TestFilters(t *testing.T) {
+	o := &clientOptions{}
+
+	WithSelectFilter(filter.Version("2"))(o)
+	assert.Equal(t, 1, len(o.filters))
+}
--- transport/grpc/server_test.go
+++ transport/grpc/server_test.go
@@ -3,26 +3,62 @@ package grpc
 import (
 	"context"
 	"crypto/tls"
+	"fmt"
 	"net/url"
 	"strings"
 	"testing"
 	"time"
 
+	"github.com/go-kratos/kratos/v2/errors"
+	pb "github.com/go-kratos/kratos/v2/internal/testdata/helloworld"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware"
-	"google.golang.org/grpc"
+	"github.com/go-kratos/kratos/v2/transport"
 
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
 )
+
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	if in.Name == "error" {
+		return nil, errors.BadRequest("custom_error", fmt.Sprintf("invalid argument %s", in.Name))
+	}
+	if in.Name == "panic" {
+		panic("server panic")
+	}
+	return &pb.HelloReply{Message: fmt.Sprintf("Hello %+v", in.Name)}, nil
+}
 
 type testKey struct{}
 
 func TestServer(t *testing.T) {
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, testKey{}, "test")
-	srv := NewServer(Middleware([]middleware.Middleware{
-		func(middleware.Handler) middleware.Handler { return nil },
-	}...))
+	srv := NewServer(
+		Middleware(
+			func(handler middleware.Handler) middleware.Handler {
+				return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+					if tr, ok := transport.FromServerContext(ctx); ok {
+						if tr.ReplyHeader() != nil {
+							tr.ReplyHeader().Set("req_id", "3344")
+						}
+					}
+					return handler(ctx, req)
+				}
+			}),
+		UnaryInterceptor(func(ctx context.Context, req interface{},
+			info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+			return handler(ctx, req)
+		}),
+		Options(grpc.InitialConnWindowSize(0)),
+	)
+	pb.RegisterGreeterServer(srv, &server{})
 
 	if e, err := srv.Endpoint(); err != nil || e == nil || strings.HasSuffix(e.Host, ":0") {
 		t.Fatal(e, err)
@@ -45,10 +81,31 @@ func testClient(t *testing.T, srv *Server) {
 		t.Fatal(err)
 	}
 	// new a gRPC client
-	conn, err := DialInsecure(context.Background(), WithEndpoint(u.Host))
+	conn, err := DialInsecure(context.Background(),
+		WithEndpoint(u.Host),
+		WithOptions(grpc.WithBlock()),
+		WithUnaryInterceptor(func(ctx context.Context, method string, req, reply interface{},
+			cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+			return invoker(ctx, method, req, reply, cc, opts...)
+		}),
+		WithMiddleware(func(handler middleware.Handler) middleware.Handler {
+			return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+				if tr, ok := transport.FromClientContext(ctx); ok {
+					header := tr.RequestHeader()
+					header.Set("x-md-trace", "2233")
+				}
+				return handler(ctx, req)
+			}
+		}),
+	)
 	if err != nil {
 		t.Fatal(err)
 	}
+	client := pb.NewGreeterClient(conn)
+	reply, err := client.SayHello(context.Background(), &pb.HelloRequest{Name: "kratos"})
+	fmt.Println(err)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello kratos", reply.Message)
 	_ = conn.Close()
 }
 
@@ -64,6 +121,10 @@ func TestAddress(t *testing.T) {
 	v := "abc"
 	Address(v)(o)
 	assert.Equal(t, v, o.address)
+
+	u, err := o.Endpoint()
+	assert.NotNil(t, err)
+	assert.Nil(t, u)
 }
 
 func TestTimeout(t *testing.T) {
