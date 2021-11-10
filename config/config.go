20ccc58f9488daff38888996667dfccda0f80dd7
Windfarer <windfarer@gmail.com>
2021-11-10 14:57:28 +0800

    add log

--- config/config.go
+++ config/config.go
@@ -103,6 +103,9 @@ func (c *config) Load() error {
 		if err != nil {
 			return err
 		}
+		for _, v := range kvs {
+			c.log.Infof("config loaded: %s format: %s", v.Key, v.Format)
+		}
 		if err = c.reader.Merge(kvs...); err != nil {
 			c.log.Errorf("failed to merge config source: %v", err)
 			return err
