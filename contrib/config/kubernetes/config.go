a25ae54900a3d1b9533ff3a6033413ee4f079bfc
Windfarer <windfarer@gmail.com>
2021-11-10 15:36:17 +0800

    goimports

--- contrib/config/kubernetes/config.go
+++ contrib/config/kubernetes/config.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 
 	"github.com/go-kratos/kratos/v2/config"
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
