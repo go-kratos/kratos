3642f5d0baf59ca97c7c5f03a0c4e2e9d490e17f
包子 <baozhecheng@foxmail.com>
2021-12-27 16:39:28 +0800

    feat(cmd/kratos): generating API documentation using Gnostic (#1716)

--- cmd/kratos/internal/upgrade/upgrade.go
+++ cmd/kratos/internal/upgrade/upgrade.go
@@ -25,6 +25,8 @@ func Run(cmd *cobra.Command, args []string) {
 		"google.golang.org/protobuf/cmd/protoc-gen-go",
 		"google.golang.org/grpc/cmd/protoc-gen-go-grpc",
 		"github.com/envoyproxy/protoc-gen-validate",
+		"github.com/google/gnostic",
+		"github.com/google/gnostic/apps/protoc-gen-openapi",
 	)
 	if err != nil {
 		fmt.Println(err)
