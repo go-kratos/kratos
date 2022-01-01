961a9694df36e3b8e9404962b0c15219e60bf5f1
zwhyb <99347745@qq.com>
2022-01-01 20:07:46 +0800

    fix lint

--- errors/wrap_test.go
+++ errors/wrap_test.go
@@ -15,9 +15,9 @@ func (*mockErr) Error() string {
 
 func TestWarp(t *testing.T) {
 	var err error = &mockErr{}
-	var err2 error = fmt.Errorf("wrap %w", err)
+	err2 := fmt.Errorf("wrap %w", err)
 	assert.Equal(t, err, Unwrap(err2))
 	assert.True(t, Is(err2, err))
-	var err3 = &mockErr{}
+	err3 := &mockErr{}
 	assert.True(t, As(err2, &err3))
 }
