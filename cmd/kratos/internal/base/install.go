11fbb4b9814e500f458ce714fa02105e4cc80819
haiyux <haiyux@foxmail.com>
2022-01-10 19:58:59 +0800

    fix

--- cmd/kratos/internal/base/install.go
+++ cmd/kratos/internal/base/install.go
@@ -7,13 +7,18 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"regexp"
 )
 
 // GoInstall go get path.
 func GoInstall(path ...string) error {
+	reg := regexp.MustCompile(`.*@v\d+[\.\d+]+$`)
 	for _, p := range path {
-		fmt.Printf("go install %s@latest\n", p)
-		cmd := exec.Command("go", "install", fmt.Sprintf("%s@latest", p))
+		if !reg.MatchString(p) {
+			p += "@latest"
+		}
+		fmt.Printf("go install %s\n", p)
+		cmd := exec.Command("go", "install", p)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		if err := cmd.Run(); err != nil {
