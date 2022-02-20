ea0b5e45c5a9c45642340556937f3176ce58502b
EndlessIdea <lockcoder@gmail.com>
2022-02-20 23:06:58 +0800

    implement kafka server

---
+++ contrib/server/kafka/consumer/consumer.go
@@ -0,0 +1,225 @@
+package consumer
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/go-kratos/kratos/v2/contrib/server/kafka"
+	"github.com/go-kratos/kratos/v2/log"
+
+	"github.com/Shopify/sarama"
+)
+
+const (
+	defaultVersion     = "2.1.0"
+	defaultMaxProcTime = 250 * time.Millisecond
+)
+
+type Message struct {
+	key   string
+	value []byte
+}
+
+func (m *Message) Key() string {
+	return m.key
+}
+
+func (m *Message) Value() []byte {
+	return m.value
+}
+
+type Consumer struct {
+	// config setting
+	brokers         []string
+	topic           string
+	group           string
+	version         string
+	balanceStrategy sarama.BalanceStrategy
+	initialOffset   int64
+	maxProcTime     time.Duration
+
+	ready chan struct{}
+
+	sarama.Client
+	consumer sarama.ConsumerGroup
+	handler  kafka.Handler
+	logger   *log.Helper
+}
+
+// ConsumerOption is a Consumer config option.
+type ConfigOption func(*Consumer)
+
+// Version with Kafka cluster version.
+func Version(version string) ConfigOption {
+	return func(c *Consumer) {
+		c.version = version
+	}
+}
+
+// BalanceStrategy with the rebalance strategy
+func BalanceStrategy(strategy sarama.BalanceStrategy) ConfigOption {
+	return func(c *Consumer) {
+		c.balanceStrategy = strategy
+	}
+}
+
+// InitialOffset with the consumer initial offset
+func InitialOffset(offset int64) ConfigOption {
+	return func(c *Consumer) {
+		c.initialOffset = offset
+	}
+}
+
+// MaxProcessingTime with the maximum amount of time the consumer expects a message takes
+func MaxProcessingTime(maxTime time.Duration) ConfigOption {
+	return func(c *Consumer) {
+		c.maxProcTime = maxTime
+	}
+}
+
+// Logger with the specify logger
+func Logger(logger log.Logger) ConfigOption {
+	return func(c *Consumer) {
+		c.logger = log.NewHelper(logger)
+	}
+}
+
+// NewConsumer inits a consumer group consumer
+func NewConsumer(brokers []string, topic, group string, opts ...ConfigOption) (*Consumer, error) {
+	result := &Consumer{
+		brokers:         brokers,
+		topic:           topic,
+		group:           group,
+		version:         defaultVersion,
+		balanceStrategy: sarama.BalanceStrategySticky,
+		initialOffset:   sarama.OffsetNewest,
+		maxProcTime:     defaultMaxProcTime,
+		ready:           make(chan struct{}),
+		logger:          log.NewHelper(log.GetLogger()),
+	}
+
+	for _, o := range opts {
+		o(result)
+	}
+
+	// check version
+	kafkaVersion, err := sarama.ParseKafkaVersion(result.version)
+	if err != nil {
+		return nil, fmt.Errorf("parse kafka version %s error: %v", result.version, err)
+	}
+
+	// init config
+	config := sarama.NewConfig()
+	config.Version = kafkaVersion
+	config.Consumer.MaxProcessingTime = result.maxProcTime
+	config.Consumer.Group.Rebalance.Strategy = result.balanceStrategy
+	config.Consumer.Offsets.Initial = result.initialOffset
+	client, err := sarama.NewClient(brokers, config)
+	if err != nil {
+		return nil, fmt.Errorf("init sarama kafka client error: %v", err)
+	}
+	consumer, err := sarama.NewConsumerGroupFromClient(group, client)
+	if err != nil {
+		return nil, fmt.Errorf("init sarama kafka group consumer error: %v", err)
+	}
+	result.Client = client
+	result.consumer = consumer
+
+	return result, nil
+}
+
+// Topics returns all the topics this consumer subscribes
+func (c *Consumer) Topic() string {
+	return c.topic
+}
+
+// RegisterHandler registers a handler to handle the messages of a specific topic
+func (c *Consumer) RegisterHandler(handler kafka.Handler) {
+	c.handler = handler
+}
+
+// RegisterHandler checks whether this consumer has a handler for the specific topic
+func (c *Consumer) HasHandler(topic string) bool {
+	return c.handler != nil
+}
+
+// Consume receives and handles messages
+func (c *Consumer) Consume(ctx context.Context) error {
+	// check handlers before consuming
+	for c.handler == nil {
+		return fmt.Errorf("consumer has no handler")
+	}
+
+	// start consuming
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			c.logger.Infof("consumer %+v session starts", c)
+
+			// `Consume` should be called inside an infinite loop, when a
+			// server-side rebalance happens, the consumer session will need to be
+			// recreated to get the new claims
+			if err := c.consumer.Consume(ctx, []string{c.topic}, c); err != nil {
+				c.logger.Errorf("consumer %+v consumes error %+v", c, err)
+				return
+			}
+
+			c.logger.Infof("consumer %+v session exits", c)
+
+			// check if context was cancelled, signaling that the consumer should stop
+			if err := ctx.Err(); err != nil {
+				c.logger.Errorf("consumer %+v exits due to context is canceled %+v", c, err)
+				return
+			}
+
+			c.ready = make(chan struct{})
+		}
+	}()
+
+	<-c.ready // Await till the consumer has been set up
+
+	wg.Wait()
+	if err := c.consumer.Close(); err != nil {
+		return fmt.Errorf("close kafka consumer error: %v", err)
+	}
+
+	return fmt.Errorf("consumer %+v exited", c.consumer)
+}
+
+func (c *Consumer) Close() error {
+	return c.consumer.Close()
+}
+
+// Setup is run at the beginning of a new session, before ConsumeClaim
+func (c *Consumer) Setup(session sarama.ConsumerGroupSession) error {
+	c.logger.Infof("consumer of group %s setup status %+v", c.group, session.Claims())
+	close(c.ready)
+	return nil
+}
+
+// Cleanup is run at the end of a session, once all ConsumeClaim goroutines have exited
+func (c *Consumer) Cleanup(session sarama.ConsumerGroupSession) error {
+	c.logger.Infof("consumer of group %s exit status %+v", c.group, session.Claims())
+	return nil
+}
+
+// ConsumeClaim must start a consumer loop of ConsumerGroupClaim's Messages().
+func (c *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	// NOTE:
+	// Do not move the code below to a goroutine.
+	// The `ConsumeClaim` itself is called within a goroutine, see:
+	// https://github.com/Shopify/sarama/blob/main/consumer_group.go#L27-L29
+	for message := range claim.Messages() {
+		if err := c.handler.Handle(&Message{key: string(message.Key), value: message.Value}); err != nil {
+			c.logger.Errorf("consume message %s of topic %s partition %d error %+v", string(message.Value), message.Topic, message.Partition, err)
+			continue
+		}
+		session.MarkMessage(message, "")
+	}
+
+	return nil
+}
--- contrib/server/kafka/server.go
+++ contrib/server/kafka/server.go
@@ -1,21 +1,3 @@
-// Copyright (c) 2012-2022 Grabtaxi Holdings PTE LTD (GRAB), All Rights Reserved. NOTICE: All information contained herein
-// is, and remains the property of GRAB. The intellectual and technical concepts contained herein are confidential, proprietary
-// and controlled by GRAB and may be covered by patents, patents in process, and are protected by trade secret or copyright law.
-//
-// You are strictly forbidden to copy, download, store (in any medium), transmit, disseminate, adapt or change this material
-// in any way unless prior written permission is obtained from GRAB. Access to the source code contained herein is hereby
-// forbidden to anyone except current GRAB employees or contractors with binding Confidentiality and Non-disclosure agreements
-// explicitly covering such access.
-//
-// The copyright notice above does not evidence any actual or intended publication or disclosure of this source code,
-// which includes information that is confidential and/or proprietary, and is a trade secret, of GRAB.
-//
-// ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC PERFORMANCE, OR PUBLIC DISPLAY OF OR THROUGH USE OF THIS SOURCE
-// CODE WITHOUT THE EXPRESS WRITTEN CONSENT OF GRAB IS STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE LAWS AND
-// INTERNATIONAL TREATIES. THE RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY
-// OR IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL ANYTHING
-// THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
-
 package kafka
 
 import (
@@ -29,6 +11,27 @@ import (
 )
 
 var _ transport.Server = (*Server)(nil)
+
+// Message is a message abstraction
+type Message interface {
+	Key() string
+	Value() []byte
+}
+
+// Handler is used to handle the received message
+type Handler interface {
+	Topic() string
+	Handle(message Message) error
+}
+
+// Consumer receives and uses the handler to process messages
+type Consumer interface {
+	Topic() string
+	RegisterHandler(handler Handler)
+	HasHandler(topic string) bool
+	Consume(ctx context.Context) error
+	Close() error
+}
 
 // Server is a Kafka server wrapper
 type Server struct {
@@ -57,7 +60,7 @@ func Handlers(handlers []Handler) ServerOption {
 }
 
 // NewServer creates a Kafka server by options.
-func NewServer(opts ...ServerOption) (*Server, error) {
+func NewServer(opts ...ServerOption) (transport.Server, error) {
 	server := &Server{handlers: make(map[string]Handler)}
 
 	for _, o := range opts {
@@ -71,16 +74,12 @@ func NewServer(opts ...ServerOption) (*Server, error) {
 		return nil, fmt.Errorf("no handlers")
 	}
 
-	for _, srvConsumer := range server.consumers {
-		for _, topic := range srvConsumer.Topics() {
-			if srvConsumer.HasHandler(topic) {
-				return nil, fmt.Errorf("duplicated handler for topic %s", topic)
+	for _, consumer := range server.consumers {
+		for _, handler := range server.handlers {
+			if consumer.Topic() != handler.Topic() {
+				continue
 			}
-			handler, ok := server.handlers[topic]
-			if !ok {
-				return nil, fmt.Errorf("no available handler for topic %s", topic)
-			}
-			srvConsumer.RegisterHandler(handler)
+			consumer.RegisterHandler(handler)
 		}
 	}
 	return server, nil
@@ -93,7 +92,9 @@ func (s *Server) Start(ctx context.Context) error {
 	for _, consumer := range s.consumers {
 		consumer := consumer
 		eg.Go(func() error {
-			return consumer.Consume(ctx)
+			for {
+				return consumer.Consume(ctx)
+			}
 		})
 	}
 
