d2f0de824016bbed26385baef3622ca16f89bfac
aliliin <phperali@gmail.com>
2022-02-24 17:40:29 +0800

    feature-examples gorm transaction

---
+++ examples/transaction/gorm/internal/biz/biz.go
@@ -0,0 +1,13 @@
+package biz
+
+import (
+    "context"
+    "github.com/google/wire"
+)
+
+// ProviderSet is biz providers.
+var ProviderSet = wire.NewSet(NewUserUsecase)
+
+type Transaction interface {
+    Transaction(context.Context, func(ctx context.Context) error) error
+}
