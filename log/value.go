71dac2d1634f3967ad7d2c9e23506ea4d8449d9f
包子 <baozhecheng@foxmail.com>
2022-04-12 16:19:44 +0800

    feat(log): add finer grained Caller control

--- log/value.go
+++ log/value.go
@@ -9,9 +9,15 @@ import (
 )
 
 var (
-	defaultDepth = 3
-	// DefaultCaller is a Valuer that returns the file and line.
+	defaultDepth = 4
+	// DefaultCaller is a Valuer that returns the file, line and func name.
 	DefaultCaller = Caller(defaultDepth)
+	// DefaultCallerFuncName is a Valuer that returns the func name.
+	DefaultCallerFuncName = CallerFuncName(defaultDepth)
+	// DefaultCallerFile is a Valuer that returns the file.
+	DefaultCallerFile = CallerFile(defaultDepth)
+	// DefaultCallerLine is a Valuer that returns the line.
+	DefaultCallerLine = CallerLine(defaultDepth)
 
 	// DefaultTimestamp is a Valuer that returns the current wallclock time.
 	DefaultTimestamp = Timestamp(time.RFC3339)
@@ -28,22 +34,62 @@ func Value(ctx context.Context, v interface{}) interface{} {
 	return v
 }
 
-// Caller returns a Valuer that returns a pkg/file:line description of the caller.
-func Caller(depth int) Valuer {
+// Caller returns a Valuer that returns a pkg/file:line,name description of the caller.
+func Caller(skip int) Valuer {
 	return func(context.Context) interface{} {
-		d := depth
-		_, file, line, _ := runtime.Caller(d)
-		if strings.LastIndex(file, "/log/filter.go") > 0 {
-			d++
-			_, file, line, _ = runtime.Caller(d)
-		}
-		if strings.LastIndex(file, "/log/helper.go") > 0 {
-			d++
-			_, file, line, _ = runtime.Caller(d)
-		}
+		pc, file, line, _ := callerDepth(skip)
 		idx := strings.LastIndexByte(file, '/')
-		return file[idx+1:] + ":" + strconv.Itoa(line)
+		name := runtime.FuncForPC(pc).Name()
+		index := strings.LastIndex(name, "/")
+		return file[idx+1:] + ":" + strconv.Itoa(line) + "," + name[index+1:]
 	}
+}
+
+// CallerFuncName returns a Valuer that returns a package.func description of the caller.
+func CallerFuncName(skip int) Valuer {
+	return func(ctx context.Context) interface{} {
+		fn, _, _, ok := callerDepth(skip)
+		if ok {
+			name := runtime.FuncForPC(fn).Name()
+			index := strings.LastIndexByte(name, '/')
+			return name[index+1:]
+		}
+		return ""
+	}
+}
+
+// CallerFile returns a Valuer that returns a pkg/file description of the caller.
+func CallerFile(skip int) Valuer {
+	return func(ctx context.Context) interface{} {
+		_, file, _, ok := callerDepth(skip)
+		if ok {
+			return file[strings.LastIndex(file, "/")+1:]
+		}
+		return ""
+	}
+}
+
+// CallerLine returns a Valuer that returns a line description of the caller.
+func CallerLine(skip int) Valuer {
+	return func(ctx context.Context) interface{} {
+		_, _, line, _ := callerDepth(skip)
+		return line
+	}
+}
+
+// CallerDepth returns skip caller.
+func callerDepth(skip int) (uintptr, string, int, bool) {
+	d := skip
+	pc, file, line, ok := runtime.Caller(skip)
+	if strings.LastIndex(file, "/log/filter.go") > 0 {
+		d++
+		pc, file, line, ok = runtime.Caller(d)
+	}
+	if strings.LastIndex(file, "/log/helper.go") > 0 {
+		d++
+		pc, file, line, ok = runtime.Caller(d)
+	}
+	return pc, file, line, ok
 }
 
 // Timestamp returns a timestamp Valuer with a custom time format.
