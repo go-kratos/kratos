0b9cf7db94a2cccca8b4d4708c492692baa61038
包子 <906604588@qq.com>
2022-04-12 16:18:14 +0800

    Update value_test.go

--- log/value_test.go
+++ log/value_test.go
@@ -1,7 +1,9 @@
 package log
 
 import (
+	"bytes"
 	"context"
+	"io"
 	"testing"
 )
 
@@ -26,5 +28,73 @@ func TestValue(t *testing.T) {
 	res := got.(int)
 	if res != 3 {
 		t.Errorf("Value() = %v, want %v", res, 3)
+	}
+}
+
+func TestCaller(t *testing.T) {
+	var buf bytes.Buffer
+	h := NewHelper(With(NewStdLogger(&buf), "caller", DefaultCaller))
+	h.Infow("args", "test")
+	want := "INFO caller=value_test.go:37,log.TestCaller args=test\n"
+	if want != buf.String() {
+		t.Fatalf("want %s，have %s", want, buf.String())
+	}
+}
+
+func TestCallerLine(t *testing.T) {
+	var buf bytes.Buffer
+	h := NewHelper(With(NewStdLogger(&buf), "callerLine", DefaultCallerLine))
+	h.Infow("args", "test")
+	want := "INFO callerLine=47 args=test\n"
+	if want != buf.String() {
+		t.Fatalf("want %s，have %s", want, buf.String())
+	}
+}
+
+func TestCallerFile(t *testing.T) {
+	var buf bytes.Buffer
+	h := NewHelper(With(NewStdLogger(&buf), "callerFile", DefaultCallerFile))
+	h.Infow("args", "test")
+	want := "INFO callerFile=value_test.go args=test\n"
+	if want != buf.String() {
+		t.Fatalf("want %s，have %s", want, buf.String())
+	}
+}
+
+func TestCallerFuncName(t *testing.T) {
+	var buf bytes.Buffer
+	h := NewHelper(With(NewStdLogger(&buf), "callerFuncName", DefaultCallerFuncName))
+	h.Infow("args", "test")
+	want := "INFO callerFuncName=log.TestCallerFuncName args=test\n"
+	if want != buf.String() {
+		t.Fatalf("want %s，have %s", want, buf.String())
+	}
+}
+
+func BenchmarkCaller(b *testing.B) {
+	h := NewHelper(With(NewStdLogger(io.Discard), "caller", DefaultCaller))
+	for i := 0; i < b.N; i++ {
+		h.Infow("args", "test")
+	}
+}
+
+func BenchmarkCallerFile(b *testing.B) {
+	h := NewHelper(With(NewStdLogger(io.Discard), "callerFile", DefaultCallerFile))
+	for i := 0; i < b.N; i++ {
+		h.Infow("args", "test")
+	}
+}
+
+func BenchmarkCallerFuncName(b *testing.B) {
+	h := NewHelper(With(NewStdLogger(io.Discard), "callerFuncName", DefaultCallerFuncName))
+	for i := 0; i < b.N; i++ {
+		h.Infow("args", "test")
+	}
+}
+
+func BenchmarkCallerLine(b *testing.B) {
+	h := NewHelper(With(NewStdLogger(io.Discard), "callerLine", DefaultCallerLine))
+	for i := 0; i < b.N; i++ {
+		h.Infow("args", "test")
 	}
 }
