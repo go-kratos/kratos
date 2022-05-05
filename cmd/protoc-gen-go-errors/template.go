48df7b1b713d70ba9e7a655d2231a06770003772
JeffreyBool <1402992668@qq.com>
2022-05-05 10:13:09 +0800

    feat:  protoc-gen-go-errors add comment (#1961)

--- cmd/protoc-gen-go-errors/template.go
+++ cmd/protoc-gen-go-errors/template.go
@@ -8,6 +8,7 @@ import (
 var errorsTemplate = `
 {{ range .Errors }}
 
+{{if .HasComment}}{{.Comment}}{{end}}
 func Is{{.CamelValue}}(err error) bool {
 	if err == nil {
 		return false
@@ -16,6 +17,7 @@ func Is{{.CamelValue}}(err error) bool {
 	return e.Reason == {{.Name}}_{{.Value}}.String() && e.Code == {{.HTTPCode}} 
 }
 
+{{if .HasComment}}{{.Comment}}{{end}}
 func Error{{.CamelValue}}(format string, args ...interface{}) *errors.Error {
 	 return errors.New({{.HTTPCode}}, {{.Name}}_{{.Value}}.String(), fmt.Sprintf(format, args...))
 }
@@ -28,6 +30,8 @@ type errorInfo struct {
 	Value      string
 	HTTPCode   int
 	CamelValue string
+	Comment    string
+	HasComment bool
 }
 
 type errorWrapper struct {
