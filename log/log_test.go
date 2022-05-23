79497dd3a59752c1f5b0534dfb90b6f8688bdcc8
Tony Chen <zhihui_chen@foxmail.com>
2022-05-24 00:40:45 +0800

    log: fix global logger (#2034)
    * log: fix global logger

--- log/log_test.go
+++ log/log_test.go
@@ -2,7 +2,6 @@ package log
 
 import (
 	"context"
-	"os"
 	"testing"
 )
 
@@ -10,14 +9,6 @@ func TestInfo(t *testing.T) {
 	logger := DefaultLogger
 	logger = With(logger, "ts", DefaultTimestamp, "caller", DefaultCaller)
 	_ = logger.Log(LevelInfo, "key1", "value1")
-}
-
-func TestWrapper(t *testing.T) {
-	out := NewStdLogger(os.Stdout)
-	err := NewStdLogger(os.Stderr)
-
-	l := With(MultiLogger(out, err), "ts", DefaultTimestamp, "caller", DefaultCaller)
-	_ = l.Log(LevelInfo, "msg", "test")
 }
 
 func TestWithContext(t *testing.T) {
