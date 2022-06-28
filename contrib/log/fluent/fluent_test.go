16a0e31aa4359713914c6d736dae5e8724d5a5cc
u5surf <u5.horie@gmail.com>
2022-06-29 08:36:21 +0900

    test(contrib): add unit test for fluent.go

--- contrib/log/fluent/fluent_test.go
+++ contrib/log/fluent/fluent_test.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/go-kratos/kratos/v2/log"
 )
@@ -29,6 +30,96 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
+func TestWithTimeout(t *testing.T) {
+	var opts = new(options)
+	var duration time.Duration = 1000000000
+	funcTimeout := WithTimeout(duration)
+	funcTimeout(opts)
+	if opts.timeout != duration {
+		t.Errorf("WithTimeout() = %v, want %v", opts.timeout, duration)
+	}
+}
+
+func TestWithWriteTimeout(t *testing.T) {
+	var opts = new(options)
+	var duration time.Duration = 1000000000
+	funcWriteTimeout := WithWriteTimeout(duration)
+	funcWriteTimeout(opts)
+	if opts.writeTimeout != duration {
+		t.Errorf("WithWriteTimeout() = %v, want %v", opts.writeTimeout, duration)
+	}
+}
+
+func TestWithBufferLimit(t *testing.T) {
+	var opts = new(options)
+	var bufferLimit int = 1000000000
+	funcBufferLimit := WithBufferLimit(bufferLimit)
+	funcBufferLimit(opts)
+	if opts.bufferLimit != bufferLimit {
+		t.Errorf("WithBufferLimit() = %d, want %d", opts.bufferLimit, bufferLimit)
+	}
+}
+
+func TestWithRetryWait(t *testing.T) {
+	var opts = new(options)
+	var retryWait int = 1000000000
+	funcRetryWait := WithRetryWait(retryWait)
+	funcRetryWait(opts)
+	if opts.retryWait != retryWait {
+		t.Errorf("WithRetryWait() = %d, want %d", opts.retryWait, retryWait)
+	}
+}
+
+func TestWithMaxRetry(t *testing.T) {
+	var opts = new(options)
+	var maxRetry int = 1000000000
+	funcMaxRetry := WithMaxRetry(maxRetry)
+	funcMaxRetry(opts)
+	if opts.maxRetry != maxRetry {
+		t.Errorf("WithMaxRetry() = %d, want %d", opts.maxRetry, maxRetry)
+	}
+}
+
+func TestWithMaxRetryWait(t *testing.T) {
+	var opts = new(options)
+	var maxRetryWait int = 1000000000
+	funcMaxRetryWait := WithMaxRetryWait(maxRetryWait)
+	funcMaxRetryWait(opts)
+	if opts.maxRetryWait != maxRetryWait {
+		t.Errorf("WithMaxRetryWait() = %d, want %d", opts.maxRetryWait, maxRetryWait)
+	}
+}
+
+func TestWithTagPrefix(t *testing.T) {
+	var opts = new(options)
+	var tagPrefix string = "tag_prefix"
+	funcTagPrefix := WithTagPrefix(tagPrefix)
+	funcTagPrefix(opts)
+	if opts.tagPrefix != tagPrefix {
+		t.Errorf("WithTagPrefix() = %s, want %s", opts.tagPrefix, tagPrefix)
+	}
+}
+
+func TestWithAsync(t *testing.T) {
+	var opts = new(options)
+	var async bool = true
+	funcAsync := WithAsync(async)
+	funcAsync(opts)
+	if opts.async != async {
+		t.Errorf("WithAsync() = %t, want %t", opts.async, async)
+	}
+}
+
+func TestWithForceStopAsyncSend(t *testing.T) {
+	var opts = new(options)
+	var forceStopAsyncSend bool = true
+	funcForceStopAsyncSend := WithForceStopAsyncSend(forceStopAsyncSend)
+	funcForceStopAsyncSend(opts)
+	if opts.forceStopAsyncSend != forceStopAsyncSend {
+		t.Errorf("WithForceStopAsyncSend() = %t, want %t", opts.forceStopAsyncSend, forceStopAsyncSend)
+	}
+}
+
 func TestLogger(t *testing.T) {
 	logger, err := NewLogger("tcp://127.0.0.1:24224")
 	if err != nil {
@@ -41,6 +132,38 @@ func TestLogger(t *testing.T) {
 	flog.Info("log", "test")
 	flog.Warn("log", "test")
 	flog.Error("log", "test")
+}
+
+func TestLoggerWithOpt(t *testing.T) {
+	var duration time.Duration = 1000000000
+	logger, err := NewLogger("tcp://127.0.0.1:24224", WithTimeout(duration))
+	if err != nil {
+		t.Error(err)
+	}
+	defer logger.Close()
+	flog := log.NewHelper(logger)
+
+	flog.Debug("log", "test")
+	flog.Info("log", "test")
+	flog.Warn("log", "test")
+	flog.Error("log", "test")
+}
+
+func TestLoggerError(t *testing.T) {
+	errCase := []string{
+		"foo",
+		"tcp://127.0.0.1/",
+		"tcp://127.0.0.1:1234a",
+		"tcp://127.0.0.1:65535",
+		"https://127.0.0.1:8080",
+		"unix://foo/bar",
+	}
+	for _, errc := range errCase {
+		_, err := NewLogger(errc)
+		if err == nil {
+			t.Error(err)
+		}
+	}
 }
 
 func BenchmarkLoggerPrint(b *testing.B) {
