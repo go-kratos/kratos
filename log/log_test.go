85af73a84beaa160cb13a811ef26b652d9434db1
jakezhu9 <107761771+jakezhu9@users.noreply.github.com>
2022-07-14 20:20:38 +0800

    test(log): increase tests coverage (#2197)

--- log/log_test.go
+++ log/log_test.go
@@ -7,7 +7,8 @@ import (
 
 func TestInfo(t *testing.T) {
 	logger := DefaultLogger
-	logger = With(logger, "ts", DefaultTimestamp, "caller", DefaultCaller)
+	logger = With(logger, "ts", DefaultTimestamp)
+	logger = With(logger, "caller", DefaultCaller)
 	_ = logger.Log(LevelInfo, "key1", "value1")
 }
 
