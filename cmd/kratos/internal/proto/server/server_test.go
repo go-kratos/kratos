9347c2bb41e28ff040179302ecf4b9ea7c233a87
hshe <hshe@163.com>
2022-07-16 13:59:52 +0800

    fix(proto/server): server .ServerName not Match to Client ServerName add serNameTest

--- cmd/kratos/internal/proto/server/server_test.go
+++ cmd/kratos/internal/proto/server/server_test.go
@@ -2,7 +2,7 @@ package server
 
 import "testing"
 
-func Test_ucFirst(t *testing.T) {
+func Test_serviceName(t *testing.T) {
 	type args struct {
 		str string
 	}
@@ -11,41 +11,27 @@ func Test_ucFirst(t *testing.T) {
 		args args
 		want string
 	}{
+
 		{
-			name: "ucFirst on lowercase words",
-			args: args{str: "helloworld"},
-			want: "Helloworld",
-		},
-		{
-			name: "ucFirst on uppercase words",
-			args: args{str: "HELLOWORLD"},
-			want: "HELLOWORLD",
-		},
-		{
-			name: "ucFirst on lowercase words with spaces",
-			args: args{str: "hello world"},
-			want: "Hello world",
-		},
-		{
-			name: "ucFirst on uppercase words with spaces",
-			args: args{str: "HELLO WORLD"},
-			want: "HELLO WORLD",
-		},
-		{
-			name: "ucFirst on Lower Camel Case words",
+			name: "serviceName on Lower Camel Case words",
 			args: args{str: "helloWorld"},
 			want: "HelloWorld",
 		},
 		{
-			name: "ucFirst on Upper Camel Case words",
+			name: "serviceName on Upper Camel Case words",
 			args: args{str: "HelloWorld"},
+			want: "HelloWorld",
+		},
+		{
+			name: "serviceName on Upper Camel Case words",
+			args: args{str: "hello_world"},
 			want: "HelloWorld",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := serviceName(tt.args.str); got != tt.want {
-				t.Errorf("ucFirst() = %v, want %v", got, tt.want)
+				t.Errorf("serviceName() = %v, want %v", got, tt.want)
 			}
 		})
 	}
