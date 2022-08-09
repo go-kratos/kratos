c407afc81d44e5ca10434427d8b6ee6da24057e7
shifengbin <shifengbin121@gmail.com>
2022-08-09 12:00:10 +0800

    fix: in case url or form bind param error should return BadRequest (#2256)
    * fix: binding url or form convert type error should be return BadRequest
    
    * golint

--- transport/http/binding/bind_test.go
+++ transport/http/binding/bind_test.go
@@ -7,6 +7,8 @@ import (
 	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/go-kratos/kratos/v2/errors"
 )
 
 func TestBindQuery(t *testing.T) {
@@ -14,7 +16,12 @@ func TestBindQuery(t *testing.T) {
 		Name string `json:"name"`
 		URL  string `json:"url"`
 	}
+
+	type TestBind2 struct {
+		Age int `json:"age"`
+	}
 	p1 := TestBind{}
+	p2 := TestBind2{}
 	type args struct {
 		vars   url.Values
 		target interface{}
@@ -34,11 +41,31 @@ func TestBindQuery(t *testing.T) {
 			wantErr: false,
 			want:    &TestBind{"kratos", "https://go-kratos.dev/"},
 		},
+		{
+			name: "test1",
+			args: args{
+				vars:   map[string][]string{"age": {"kratos"}, "url": {"https://go-kratos.dev/"}},
+				target: &p2,
+			},
+			wantErr: true,
+			want:    errors.BadRequest("CODEC", ""),
+		},
+		{
+			name: "test2",
+			args: args{
+				vars:   map[string][]string{"age": {"1"}, "url": {"https://go-kratos.dev/"}},
+				target: &TestBind2{},
+			},
+			wantErr: false,
+			want:    &TestBind2{Age: 1},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := BindQuery(tt.args.vars, tt.args.target); (err != nil) != tt.wantErr {
 				t.Errorf("BindQuery() error = %v, wantErr %v", err, tt.wantErr)
+			} else {
+				t.Log(err)
 			}
 			if !tt.wantErr && !reflect.DeepEqual(tt.args.target, tt.want) {
 				t.Errorf("BindQuery() target = %v, want %v", tt.args.target, tt.want)
@@ -51,6 +78,9 @@ func TestBindForm(t *testing.T) {
 	type TestBind struct {
 		Name string `json:"name"`
 		URL  string `json:"url"`
+	}
+	type TestBind2 struct {
+		Age int `json:"age"`
 	}
 	p1 := TestBind{}
 	type args struct {
@@ -85,6 +115,19 @@ func TestBindForm(t *testing.T) {
 			wantErr: false,
 			want:    &TestBind{"kratos", "https://go-kratos.dev/"},
 		},
+		{
+			name: "error BadRequest",
+			args: args{
+				req: &http.Request{
+					Method: "POST",
+					Header: http.Header{"Content-Type": {"application/x-www-form-urlencoded; param=value"}},
+					Body:   io.NopCloser(strings.NewReader("age=a")),
+				},
+				target: &TestBind2{},
+			},
+			wantErr: true,
+			want:    nil,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -92,6 +135,7 @@ func TestBindForm(t *testing.T) {
 			if (err != nil) != tt.wantErr {
 				t.Errorf("BindForm() error = %v, wantErr %v", err, tt.wantErr)
 			}
+			t.Log(err)
 			if !tt.wantErr && !reflect.DeepEqual(tt.args.target, tt.want) {
 				t.Errorf("BindForm() target = %v, want %v", tt.args.target, tt.want)
 			}
