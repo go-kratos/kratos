9579dd325518ac03d415f6fa36d7a43e635bd929
Kagaya <kagaya85@outlook.com>
2022-08-10 14:45:49 +0800

    fix lint error

--- transport/grpc/resolver/direct/builder.go
+++ transport/grpc/resolver/direct/builder.go
@@ -14,7 +14,8 @@ type directBuilder struct{}
 
 // NewBuilder creates a directBuilder which is used to factory direct resolvers.
 // example:
-//   direct://<authority>/127.0.0.1:9000,127.0.0.2:9000
+//
+//	direct://<authority>/127.0.0.1:9000,127.0.0.2:9000
 func NewBuilder() resolver.Builder {
 	return &directBuilder{}
 }
