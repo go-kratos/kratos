a43c314ae53a22f3e98fd571d05e5dab584571ff
E01060 <hyzhao@birentech.com>
2022-08-17 13:38:14 +0800

    fix lint

--- internal/endpoint/endpoint_test.go
+++ internal/endpoint/endpoint_test.go
@@ -6,40 +6,6 @@ import (
 	"testing"
 )
 
-func TestEndPoint(t *testing.T) {
-	type args struct {
-		url *url.URL
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		// TODO: Add test cases.
-		{
-			name: "grpc://127.0.0.1?isSecure=false",
-			args: args{&url.URL{Scheme: "grpc", Host: "127.0.0.1", RawQuery: "isSecure=false"}},
-			want: false,
-		},
-		{
-			name: "grpc://127.0.0.1?isSecure=true",
-			args: args{&url.URL{Scheme: "grpc", Host: "127.0.0.1", RawQuery: "isSecure=true"}},
-			want: true,
-		},
-		{
-			name: "grpc://127.0.0.1",
-			args: args{&url.URL{Scheme: "grpc", Host: "127.0.0.1"}},
-			want: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := IsSecure(tt.args.url); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetQuery() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
 func TestNewEndpoint(t *testing.T) {
 	type args struct {
