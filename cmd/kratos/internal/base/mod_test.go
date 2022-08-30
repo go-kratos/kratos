4c1f30e1e353bb84e2994847de78a97d85977d40
180909 <734461790@qq.com>
2022-08-30 14:11:23 +0800

    test(cmd): add ModulePath test

--- cmd/kratos/internal/base/mod_test.go
+++ cmd/kratos/internal/base/mod_test.go
@@ -1,6 +1,9 @@
 package base
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestModuleVersion(t *testing.T) {
 	v, err := ModuleVersion("golang.org/x/mod")
@@ -8,4 +11,33 @@ func TestModuleVersion(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Log(v)
+}
+
+func TestModulePath(t *testing.T) {
+	if err := os.Mkdir("/tmp/test_mod", os.ModePerm); err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.RemoveAll("/tmp/test_mod")
+
+	f, err := os.Create("/tmp/test_mod/go.mod")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	mod := `module github.com/go-kratos/kratos/v2
+
+go 1.16`
+	_, err = f.WriteString(mod)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	p, err := ModulePath("/tmp/test_mod/go.mod")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if p != "github.com/go-kratos/kratos/v2" {
+		t.Fatalf("want: %s, got: %s", "module github.com/go-kratos/kratos/v2", p)
+	}
 }
