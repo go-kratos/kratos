c221f283318bd25ed907427e2cdab9b4f9d97b2e
demoManito <1430482733@qq.com>
2022-09-02 14:27:36 +0800

    fix: Log print duplicate key val

--- log/log.go
+++ log/log.go
@@ -3,6 +3,7 @@ package log
 import (
 	"context"
 	"log"
+	"reflect"
 )
 
 // DefaultLogger is default logger.
@@ -27,10 +28,35 @@ func (c *logger) Log(level Level, keyvals ...interface{}) error {
 		bindValues(c.ctx, kvs)
 	}
 	kvs = append(kvs, keyvals...)
-	if err := c.logger.Log(level, kvs...); err != nil {
+	if err := c.logger.Log(level, uniqKeys(kvs)...); err != nil {
 		return err
 	}
 	return nil
+}
+
+// uniqKeys Under the same key, the Val corresponding to the last key shall prevail
+func uniqKeys(kvs []interface{}) []interface{} {
+	if len(kvs) == 0 {
+		return kvs
+	}
+	if len(kvs)&1 == 1 {
+		kvs = append(kvs, "KEYVALS UNPAIRED") // key missing val, add a virtual value
+	}
+	uniqMap := make(map[reflect.Value]interface{})
+	keys := make([]interface{}, 0, len(kvs))
+	for i := 0; i < len(kvs); i = i + 2 {
+		key, val := kvs[i], kvs[i+1]
+		rvKey := reflect.ValueOf(key) // key missing val, map cannot be stored log.Valuer, so reflect.Value is used as the key here
+		if _, ok := uniqMap[rvKey]; !ok {
+			keys = append(keys, key) // ensure the data sequence is consistent
+		}
+		uniqMap[rvKey] = val // ensure that the val corresponding to the key is the latest
+	}
+	uniq := make([]interface{}, 0, 2*len(keys))
+	for _, key := range keys {
+		uniq = append(uniq, key, uniqMap[reflect.ValueOf(key)])
+	}
+	return uniq
 }
 
 // With with logger fields.
--- log/log_test.go
+++ log/log_test.go
@@ -2,13 +2,22 @@ package log
 
 import (
 	"context"
+	"reflect"
 	"testing"
 )
+
+func TestUniqKeys(t *testing.T) {
+	uniq := uniqKeys([]interface{}{"k1", "v1", "k1", "vv1", "k1", "vvv1", "k2", "v2"})
+	if !reflect.DeepEqual(uniq, []interface{}{"k1", "vvv1", "k2", "v2"}) {
+		t.Error("inconsistent data after deduplication")
+	}
+}
 
 func TestInfo(t *testing.T) {
 	logger := DefaultLogger
 	logger = With(logger, "ts", DefaultTimestamp)
 	logger = With(logger, "caller", DefaultCaller)
+	logger = With(logger, "caller", Caller(1))
 	_ = logger.Log(LevelInfo, "key1", "value1")
 }
 
