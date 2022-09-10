375e965a1e518708bc5e9bf75e102b78992efe3b
demoManito <1430482733@qq.com>
2022-09-10 11:53:56 +0800

    feat: remote assert libraries

--- contrib/registry/servicecomb/registry_test.go
+++ contrib/registry/servicecomb/registry_test.go
@@ -6,17 +6,14 @@ import (
 
 	pb "github.com/go-chassis/cari/discovery"
 	"github.com/go-chassis/sc-client"
-
 	"github.com/go-kratos/kratos/v2/registry"
 	"github.com/gofrs/uuid"
-	"github.com/stretchr/testify/assert"
 )
 
 var r *Registry
 
 func init() {
-	c := &mockClient{}
-	r = NewRegistry(c)
+	r = NewRegistry(&mockClient{})
 }
 
 type mockClient struct{}
@@ -28,7 +25,6 @@ func (receiver *mockClient) WatchMicroService(microServiceID string, callback fu
 //nolint
 func (receiver *mockClient) FindMicroServiceInstances(consumerID,
 	appID, microServiceName, versionRule string, opts ...sc.CallOption,
-
 ) ([]*pb.MicroServiceInstance, error) {
 	if microServiceName == "KratosServicecomb" {
 		return []*pb.MicroServiceInstance{{}}, nil
@@ -56,10 +52,11 @@ func (receiver *mockClient) GetMicroServiceID(appID, microServiceName, version, 
 	return "", nil
 }
 
-// TestRegistry
 func TestRegistry(t *testing.T) {
 	instanceID, err := uuid.NewV4()
-	assert.NoError(t, err)
+	if err != nil {
+		t.Fatal(err)
+	}
 	svc := &registry.ServiceInstance{
 		Name:      "KratosServicecomb",
 		Version:   "0.0.1",
@@ -70,25 +67,34 @@ func TestRegistry(t *testing.T) {
 	ctx := context.TODO()
 	t.Run("Register test, expected: success.", func(t *testing.T) {
 		err = r.Register(ctx, svc)
-		assert.NoError(t, err)
+		if err != nil {
+			t.Error(err)
+		}
 	})
 	t.Run("GetService test, expected: success.", func(t *testing.T) {
 		var insts []*registry.ServiceInstance
 		insts, err = r.GetService(ctx, svc.Name)
-		assert.NoError(t, err)
-		assert.Greater(t, len(insts), 0)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(insts) <= 0 {
+			t.Errorf("inst len less than 0")
+		}
 	})
 	t.Run("Deregister test, expected: success.", func(t *testing.T) {
 		svc.ID = instanceID.String()
 		err = r.Deregister(ctx, svc)
-		assert.NoError(t, err)
+		if err != nil {
+			t.Error(err)
+		}
 	})
 }
 
 func TestWatcher(t *testing.T) {
-	ctx := context.TODO()
 	instanceID1, err := uuid.NewV4()
-	assert.NoError(t, err)
+	if err != nil {
+		t.Fatal(err)
+	}
 	svc1 := &registry.ServiceInstance{
 		Name:      "WatcherTest",
 		Version:   "0.0.1",
@@ -96,33 +102,56 @@ func TestWatcher(t *testing.T) {
 		Endpoints: []string{"tcp://127.0.0.1:9000?isSecure=false"},
 		ID:        instanceID1.String(),
 	}
+	ctx := context.TODO()
 	err = r.Register(ctx, svc1)
-	assert.NoError(t, err)
+	if err != nil {
+		t.Fatal(err)
+	}
 	w, err := r.Watch(ctx, "WatcherTest")
-	assert.NoError(t, err)
-	assert.NotEmpty(t, w)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if w == nil {
+		t.Fatal("w is nil")
+	}
 	sbWatcher := w.(*Watcher)
 	t.Run("Watch register event, expected: success", func(t *testing.T) {
 		go sbWatcher.Put(svc1)
 		var instances []*registry.ServiceInstance
 		instances, err = w.Next()
-		assert.NoError(t, err)
-		assert.NotEmpty(t, instances)
-		assert.Equal(t, instanceID1.String(), instances[0].ID)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(instances) == 0 {
+			t.Errorf("instances is empty")
+		}
+		if instanceID1.String() != instances[0].ID {
+			t.Errorf("expected %v, got %v", instanceID1.String(), instances[0].ID)
+		}
 	})
 	t.Run("Watch deregister event, expected: success", func(t *testing.T) {
 		// Deregister instance1.
 		err = r.Deregister(ctx, svc1)
-		assert.NoError(t, err)
+		if err != nil {
+			t.Fatal(err)
+		}
 		go sbWatcher.Put(svc1)
 		var instances []*registry.ServiceInstance
 		instances, err = w.Next()
-		assert.NoError(t, err)
-		assert.NotEmpty(t, instances)
-		assert.Equal(t, instanceID1.String(), instances[0].ID)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(instances) == 0 {
+			t.Errorf("instances is empty")
+		}
+		if instanceID1.String() != instances[0].ID {
+			t.Errorf("expected %v, got %v", instanceID1.String(), instances[0].ID)
+		}
 	})
 	t.Run("Stop test, expected: success", func(t *testing.T) {
 		err = w.Stop()
-		assert.NoError(t, err)
+		if err != nil {
+			t.Error(err)
+		}
 	})
 }
