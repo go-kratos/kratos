f9825cb900fdc63669c0a6654b41dfba27992d67
chenzhihui <zhihui_chen@foxmail.com>
2022-10-10 21:04:47 +0800

    fix ttl

--- cache/cache.go
+++ cache/cache.go
@@ -8,6 +8,6 @@ import (
 // Cache is a generic key value cache interface.
 type Cache interface {
 	Get(ctx context.Context, key string) (interface{}, error)
-	Put(ctx context.Context, key string, value interface{}, expired time.Duration) error
+	Put(ctx context.Context, key string, value interface{}, ttl time.Duration) error
 	Delete(ctx context.Context, key string) error
 }
