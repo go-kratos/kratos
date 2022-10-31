764fb76419158d5f0b60ae3c74162d810594c32b
chenzhihui <zhihui_chen@foxmail.com>
2022-11-01 00:07:53 +0800

    add health check

---
+++ health/health.go
@@ -0,0 +1,78 @@
+package health
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+type Option func(*Health)
+
+type Status string
+
+const (
+	StatusUp   = Status("UP")
+	StatusDown = Status("DOWN")
+)
+
+type Result struct {
+	Status Status `json:"status"`
+	Error  string `json:"error"`
+}
+
+// Health .
+type Health struct {
+	liveness map[string]Checker
+	readness map[string]Checker
+}
+
+func New() *Health {
+	return &Health{}
+}
+
+func (h *Health) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var (
+		res struct {
+			Status  Status
+			Details map[string]Result `json:"details"`
+		}
+	)
+	res.Status = StatusUp
+	for n, c := range h.liveness {
+		if err := c.CheckHealth(r.Context()); err != nil {
+			res.Status = StatusDown
+			res.Details[n] = Result{Status: StatusDown, Error: err.Error()}
+		} else {
+			res.Details[n] = Result{Status: StatusUp}
+		}
+	}
+	for n, c := range h.liveness {
+		if err := c.CheckHealth(r.Context()); err != nil {
+			res.Details[n] = Result{StatusUp, err.Error()}
+		} else {
+			res.Details[n] = Result{Status: StatusUp}
+		}
+	}
+	if res.Status == StatusDown {
+		w.WriteHeader(http.StatusInternalServerError)
+	} else {
+		w.WriteHeader(http.StatusOK)
+	}
+	json.NewEncoder(w).Encode(res)
+}
+
+// Checker returns nil if the resource is healthy, or a non-nil
+// error if the resource is not healthy.
+type Checker interface {
+	CheckHealth(ctx context.Context) error
+}
+
+// CheckerFunc is an adapter type to allow the use of ordinary functions as
+// health checks. If f is a function with the appropriate signature,
+// CheckerFunc(f) is a Checker that calls f.
+type CheckerFunc func() error
+
+// CheckHealth calls f().
+func (f CheckerFunc) CheckHealth() error {
+	return f()
+}
