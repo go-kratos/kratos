19f008b48379f0d8f0802da087d055690875e58d
hoslo <952572463@qq.com>
2023-02-23 18:26:23 +0800

    feat: Add latency to the recover handler context (#2653)
    * feat: Make recovey log information more abundant
    
    * feat: Add latency to the recover handler context
    
    * feat: Add latency to the recover handler context
    
    * feat: Add latency to the recover handler context
    
    ---------
    
    Co-authored-by: shuai_yang <shuai_yang@intsig.net>

--- middleware/recovery/recovery.go
+++ middleware/recovery/recovery.go
@@ -3,11 +3,15 @@ package recovery
 import (
 	"context"
 	"runtime"
+	"time"
 
 	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware"
 )
+
+// Latency is recovery latency context key
+type Latency struct{}
 
 // ErrUnknownRequest is unknown request error.
 var ErrUnknownRequest = errors.InternalServer("UNKNOWN", "unknown request error")
@@ -41,13 +45,14 @@ func Recovery(opts ...Option) middleware.Middleware {
 	}
 	return func(handler middleware.Handler) middleware.Handler {
 		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+			startTime := time.Now()
 			defer func() {
 				if rerr := recover(); rerr != nil {
 					buf := make([]byte, 64<<10) //nolint:gomnd
 					n := runtime.Stack(buf, false)
 					buf = buf[:n]
 					log.Context(ctx).Errorf("%v: %+v\n%s\n", rerr, req, buf)
-
+					ctx = context.WithValue(ctx, Latency{}, time.Since(startTime).Seconds())
 					err = op.handler(ctx, req, rerr)
 				}
 			}()
