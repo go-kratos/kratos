93532ae621887c83e1f65790ea83613983c85d9c
GuangZhan <guang.zhan@zoom.us>
2023-06-14 11:09:42 +0800

    fix(config): references between different sources are consistent when watching
    Refs #2801

--- config/config_test.go
+++ config/config_test.go
@@ -182,3 +182,74 @@ func TestConfig(t *testing.T) {
 		t.Error("len(testConf.Endpoints) is not equal to 2")
 	}
 }
+
+func AssertStringValue(t *testing.T, expect any, v Value) bool {
+	t.Helper()
+	vs, err := v.String()
+	if err != nil {
+		t.Errorf("get string value error: %s", err.Error())
+		return false
+	}
+	if vs != expect {
+		t.Errorf("value '%s' not equal '%s'", vs, expect)
+		return false
+	}
+	return true
+}
+
+func TestConfig_WatchBetweenSourcesReferenceConfig(t *testing.T) {
+	var testJson = `
+{
+	"foo": "${remote.foo}"
+}
+`
+	var testJson2 = `
+{
+	"remote": {
+		"foo": "bar"
+	}
+}
+`
+	var testJsonUpdate = `
+{
+	"remote": {
+		"foo": "bar2"
+	}
+}
+`
+	var src1 = newTestJSONSource(testJson)
+	var src2 = newTestJSONSource(testJson2)
+	opts := options{
+		sources:  []Source{src1, src2},
+		decoder:  defaultDecoder,
+		resolver: defaultResolver,
+	}
+	cf := &config{}
+	cf.opts = opts
+	cf.reader = newReader(opts)
+
+	// load config
+	if err := cf.Load(); err != nil {
+		t.Fatal(err)
+	}
+
+	if !AssertStringValue(t, "bar", cf.Value("foo")) {
+		t.FailNow()
+	}
+	if !AssertStringValue(t, "bar", cf.Value("remote.foo")) {
+		t.FailNow()
+	}
+
+	// update remote.foo value
+	src2.data = testJsonUpdate
+	src2.sig <- struct{}{}
+	// wait for watch to finish
+	src2.sig <- struct{}{}
+
+	if !AssertStringValue(t, "bar2", cf.Value("foo")) {
+		t.FailNow()
+	}
+	if !AssertStringValue(t, "bar2", cf.Value("remote.foo")) {
+		t.FailNow()
+	}
+}
