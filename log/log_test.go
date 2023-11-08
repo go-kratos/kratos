8cc10c1f0188a83c2bafbaab8ac32504d3cd7a87
ionling <soarvision@outlook.com>
2023-11-08 16:46:40 +0800

    test(log): add concurrence test for `WithContext()`

--- log/log_test.go
+++ log/log_test.go
@@ -1,8 +1,11 @@
 package log
 
 import (
+	"bytes"
 	"context"
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestInfo(_ *testing.T) {
@@ -12,6 +15,48 @@ func TestInfo(_ *testing.T) {
 	_ = logger.Log(LevelInfo, "key1", "value1")
 }
 
-func TestWithContext(_ *testing.T) {
-	WithContext(context.Background(), nil)
+type traceIDKey struct{}
+
+func setTraceID(ctx context.Context, tid string) context.Context {
+	return context.WithValue(ctx, traceIDKey{}, tid)
+}
+
+func traceIDValuer() Valuer {
+	return func(ctx context.Context) any {
+		if ctx == nil {
+			return ""
+		}
+		return ctx.Value(traceIDKey{})
+	}
+}
+
+func TestWithContext(t *testing.T) {
+	var buf bytes.Buffer
+	pctx := context.Background()
+	l := NewFilter(
+		With(NewStdLogger(&buf), "trace-id", traceIDValuer()),
+		FilterLevel(LevelInfo),
+	)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		time.Sleep(time.Second)
+		NewHelper(l).Info("done1")
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		tid := "world"
+		ctx := setTraceID(pctx, tid)
+		NewHelper((WithContext(ctx, l))).Info("done2")
+	}()
+
+	wg.Wait()
+	expected := "INFO trace-id=world msg=done2\nINFO trace-id= msg=done1\n"
+	if got := buf.String(); got != expected {
+		t.Errorf("got: %#v", got)
+	}
 }
