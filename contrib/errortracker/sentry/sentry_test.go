1c93de04f2103d5bba248828b32513547743c09f
Windfarer <windfarer@gmail.com>
2023-12-16 16:02:35 +0800

    add test

--- contrib/errortracker/sentry/sentry_test.go
+++ contrib/errortracker/sentry/sentry_test.go
@@ -1,1 +1,16 @@
 package sentry
+
+import "testing"
+
+func TestWithTags(t *testing.T) {
+	opts := new(options)
+	strval := "bar"
+	kvs := map[string]interface{}{
+		"foo": strval,
+	}
+	funcTags := WithTags(kvs)
+	funcTags(opts)
+	if opts.tags["foo"].(string) != strval {
+		t.Errorf("TestWithTags() = %s, want %s", opts.tags["foo"].(string), strval)
+	}
+}
