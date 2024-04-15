60b4d0cafada37f39b8faf1f9182193d036cbf91
chenyurun01 <chenyurun01@bilibili.com>
2024-04-15 15:54:20 +0800

    修复 map.path 映射1

--- encoding/form/proto_decode.go
+++ encoding/form/proto_decode.go
@@ -56,7 +56,7 @@ func populateFieldValues(v protoreflect.Message, fieldPath []string, values []st
 		if fd.Message() == nil || fd.Cardinality() == protoreflect.Repeated {
 			if fd.IsMap() && len(fieldPath) > 1 {
 				// post subfield
-				return populateMapField(fd, v.Mutable(fd).Map(), []string{fieldPath[1]}, values)
+				return populateMapField(fd, v.Mutable(fd).Map(), []string{fieldPath[0]}, values)
 			}
 			return fmt.Errorf("invalid path: %q is not a message", fieldName)
 		}
