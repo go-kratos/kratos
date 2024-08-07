ac1b5c5f93ec86d066090c0976508cd7827a1149
Abhishek koserwal <akoserwa@redhat.com>
2024-08-07 12:06:26 +0530

    removing stream middleware and stream matcher impl

--- internal/matcher/middleware.go
+++ internal/matcher/middleware.go
@@ -10,36 +10,25 @@ import (
 // Matcher is a middleware matcher.
 type Matcher interface {
 	Use(ms ...middleware.Middleware)
-	UseStream(ms ...middleware.StreamMiddleware)
 	Add(selector string, ms ...middleware.Middleware)
 	Match(operation string) []middleware.Middleware
-	AddStream(selector string, ms ...middleware.StreamMiddleware)
-	MatchStream(operation string) []middleware.StreamMiddleware
 }
 
 // New new a middleware matcher.
 func New() Matcher {
 	return &matcher{
-		matchs:       make(map[string][]middleware.Middleware),
-		streamMatchs: make(map[string][]middleware.StreamMiddleware),
+		matchs: make(map[string][]middleware.Middleware),
 	}
 }
 
 type matcher struct {
-	prefix         []string
-	streamPrefix   []string
-	defaults       []middleware.Middleware
-	streamDefaults []middleware.StreamMiddleware
-	matchs         map[string][]middleware.Middleware
-	streamMatchs   map[string][]middleware.StreamMiddleware
+	prefix   []string
+	defaults []middleware.Middleware
+	matchs   map[string][]middleware.Middleware
 }
 
 func (m *matcher) Use(ms ...middleware.Middleware) {
 	m.defaults = ms
-}
-
-func (m *matcher) UseStream(ms ...middleware.StreamMiddleware) {
-	m.streamDefaults = ms
 }
 
 func (m *matcher) Add(selector string, ms ...middleware.Middleware) {
@@ -56,20 +45,6 @@ func (m *matcher) Add(selector string, ms ...middleware.Middleware) {
 	m.matchs[selector] = ms
 }
 
-func (m *matcher) AddStream(selector string, ms ...middleware.StreamMiddleware) {
-	if strings.HasSuffix(selector, "*") {
-		selector = strings.TrimSuffix(selector, "*")
-		m.streamPrefix = append(m.streamPrefix, selector)
-		// sort the prefix:
-		//  - /foo/bar
-		//  - /foo
-		sort.Slice(m.streamPrefix, func(i, j int) bool {
-			return m.streamPrefix[i] > m.streamPrefix[j]
-		})
-	}
-	m.streamMatchs[selector] = ms
-}
-
 func (m *matcher) Match(operation string) []middleware.Middleware {
 	ms := make([]middleware.Middleware, 0, len(m.defaults))
 	if len(m.defaults) > 0 {
@@ -81,22 +56,6 @@ func (m *matcher) Match(operation string) []middleware.Middleware {
 	for _, prefix := range m.prefix {
 		if strings.HasPrefix(operation, prefix) {
 			return append(ms, m.matchs[prefix]...)
-		}
-	}
-	return ms
-}
-
-func (m *matcher) MatchStream(operation string) []middleware.StreamMiddleware {
-	ms := make([]middleware.StreamMiddleware, 0, len(m.streamDefaults))
-	if len(m.streamDefaults) > 0 {
-		ms = append(ms, m.streamDefaults...)
-	}
-	if next, ok := m.streamMatchs[operation]; ok {
-		return append(ms, next...)
-	}
-	for _, prefix := range m.streamPrefix {
-		if strings.HasPrefix(operation, prefix) {
-			return append(ms, m.streamMatchs[prefix]...)
 		}
 	}
 	return ms
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -2,34 +2,17 @@ package middleware
 
 import (
 	"context"
-	"google.golang.org/grpc"
 )
 
 // Handler defines the handler invoked by Middleware.
 type Handler func(ctx context.Context, req interface{}) (interface{}, error)
 
-// StreamHandler defines the handler invoked by Middleware for stream calls.
-type StreamHandler func(srv interface{}, stream grpc.ServerStream) error
-
 // Middleware is HTTP/gRPC transport middleware.
 type Middleware func(Handler) Handler
-
-// StreamMiddleware is gRPC stream transport middleware.
-type StreamMiddleware func(StreamHandler) StreamHandler
 
 // Chain returns a Middleware that specifies the chained handler for endpoint.
 func Chain(m ...Middleware) Middleware {
 	return func(next Handler) Handler {
-		for i := len(m) - 1; i >= 0; i-- {
-			next = m[i](next)
-		}
-		return next
-	}
-}
-
-// ChainStream returns a StreamMiddleware that specifies the chained handler for endpoint.
-func ChainStream(m ...StreamMiddleware) StreamMiddleware {
-	return func(next StreamHandler) StreamHandler {
 		for i := len(m) - 1; i >= 0; i-- {
 			next = m[i](next)
 		}
