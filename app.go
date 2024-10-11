7fbdc81fe8fa1de3bb0493e1171f2875f1eee590
Monet Lee <monet_lee@163.com>
2024-10-11 10:05:03 +0800

    update field name to server. aviod copyloopvar check.

--- app.go
+++ app.go
@@ -99,16 +99,17 @@ func (a *App) Run() error {
 		}
 	}
 	for _, srv := range a.opts.servers {
+		server := srv
 		eg.Go(func() error {
 			<-ctx.Done() // wait for stop signal
 			stopCtx, cancel := context.WithTimeout(NewContext(a.opts.ctx, a), a.opts.stopTimeout)
 			defer cancel()
-			return srv.Stop(stopCtx)
+			return server.Stop(stopCtx)
 		})
 		wg.Add(1)
 		eg.Go(func() error {
 			wg.Done() // here is to ensure server start has begun running before register, so defer is not needed
-			return srv.Start(NewContext(a.opts.ctx, a))
+			return server.Start(NewContext(a.opts.ctx, a))
 		})
 	}
 	wg.Wait()
