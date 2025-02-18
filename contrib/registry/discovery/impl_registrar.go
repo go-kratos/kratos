2035b68b35d236060a7cf035915769623e3acf34
huangzw <huangzw@2345.com>
2025-02-18 17:49:08 +0800

    chore: use the upper-level passed ctx in Discovery.cancel

--- contrib/registry/discovery/impl_registrar.go
+++ contrib/registry/discovery/impl_registrar.go
@@ -52,7 +52,7 @@ func (d *Discovery) Register(ctx context.Context, service *registry.ServiceInsta
 			case <-ticker.C:
 				_ = d.renew(ctx, ins)
 			case <-ctx.Done():
-				_ = d.cancel(ins)
+				_ = d.cancel(ctx, ins)
 				ch <- struct{}{}
 				return
 			}
@@ -122,7 +122,7 @@ func (d *Discovery) register(ctx context.Context, ins *discoveryInstance) (err e
 	return
 }
 
-func (d *Discovery) Deregister(_ context.Context, service *registry.ServiceInstance) error {
+func (d *Discovery) Deregister(ctx context.Context, service *registry.ServiceInstance) error {
 	ins := fromServerInstance(service, d.config)
-	return d.cancel(ins)
+	return d.cancel(ctx, ins)
 }
