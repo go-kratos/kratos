16fb38e70dc0af5b76aa587f7494deaa5d90acd7
chenzhihui <zhihui_chen@foxmail.com>
2025-12-17 13:57:27 +0800

    simplify Validator function signature

--- middleware/validate/validate.go
+++ middleware/validate/validate.go
@@ -10,32 +10,13 @@ import (
 // ValidateFunc defines a validation function type.
 type ValidateFunc func(v any) error
 
-// Option defines a function type for configuring options.
-type Option func(*options)
-
-// WithValidator adds a custom validator function.
-func WithValidator(vs ...ValidateFunc) Option {
-	return func(o *options) {
-		o.validators = vs
-	}
-}
-
-// options holds the configuration for the validator middleware.
-type options struct {
-	validators []ValidateFunc
-}
-
 // requestValidator is an interface for types that can validate themselves.
 type requestValidator interface {
 	Validate() error
 }
 
 // Validator is a validator middleware.
-func Validator(opts ...Option) middleware.Middleware {
-	var o options
-	for _, opt := range opts {
-		opt(&o)
-	}
+func Validator(validators ...ValidateFunc) middleware.Middleware {
 	return func(handler middleware.Handler) middleware.Handler {
 		return func(ctx context.Context, req any) (reply any, err error) {
 			if v, ok := req.(requestValidator); ok {
@@ -43,7 +24,7 @@ func Validator(opts ...Option) middleware.Middleware {
 					return nil, errors.BadRequest("VALIDATOR", err.Error()).WithCause(err)
 				}
 			}
-			for _, v := range o.validators {
+			for _, v := range validators {
 				if err := v(req); err != nil {
 					return nil, errors.BadRequest("VALIDATOR", err.Error()).WithCause(err)
 				}
