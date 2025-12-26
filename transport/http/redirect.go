764398fdbaf8d914042515e8fa5141e2892f33ca
chenzhihui <zhihui_chen@foxmail.com>
2025-12-26 16:39:16 +0800

    add NewRedirectError function

--- transport/http/redirect.go
+++ transport/http/redirect.go
@@ -22,6 +22,11 @@ func (r *redirect) Error() string {
 // The provided code should be in the 3xx range and is usually StatusMovedPermanently, StatusFound or StatusSeeOther.
 // If the Content-Type header has not been set, Redirect sets it to "text/html; charset=utf-8" and writes a small HTML body.
 // Setting the Content-Type header to any value, including nil, disables that behavior.
-func NewRedirect(url string, code int) *redirect {
+func NewRedirect(url string, code int) Redirector {
+	return &redirect{URL: url, Code: code}
+}
+
+// NewRedirectError new a redirect error with url, which may be a path relative to the request path.
+func NewRedirectError(url string, code int) error {
 	return &redirect{URL: url, Code: code}
 }
